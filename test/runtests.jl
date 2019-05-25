07ac7f685a9c177f58a26bcafa3c9b36a899425d
Julian P Samaroo <jpsamaroo@jpsamaroo.me>
2019-05-25 00:00:00 -0500

    Test only when devices available

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,42 +2,54 @@ using HSARuntime
 using Test
 
 if HSARuntime.configured
-    HSARuntime.set_default_agent!(:gpu)
-    agent = get_default_agent()
-    agent_name = HSARuntime.get_name(agent)
-    @test length(agent_name) > 0
-    agent_isa = get_first_isa(agent)
-    @test length(agent_isa) > 0
-    @info "Testing with agent $agent_name with ISA $agent_isa"
+    agents = get_agents()
+    if length(agents) > 0
+        agent = get_default_agent()
+        @test agent !== nothing
 
-    arr = HSAArray(agent, UInt32, (8, ))
-    fill!(arr, UInt32(1))
-    _arr = Array(arr)
-    @test length(arr) == length(_arr)
-    for idx in 1:length(arr)
-        _arr[idx] = arr[idx]
-    end
-    @test arr == _arr
-    fill!(_arr, UInt32(2))
-    for idx in 1:length(arr)
-        arr[idx] = _arr[idx]
-    end
-    @test arr == _arr
+        HSARuntime.set_default_agent!(:gpu)
+        agent = get_default_agent()
+        @test HSARuntime.device_type(agent) == :gpu
 
-    arr1 = HSAArray(agent, UInt32, (8,))
-    arr2 = HSAArray(agent, UInt32, (8,))
-    fill!(arr1, 1)
-    fill!(arr2, 2)
-    arr3 = similar(arr1)
-    for i in 1:length(arr1)
-        arr3[i] = arr1[i] + arr2[i]
-    end
-    @test all(arr3 .== 3)
-    @test all(isa.((arr1, arr2, arr3), Ref(HSAArray)))
-    @test size(arr3) == size(arr1)
+        agent_name = HSARuntime.get_name(agent)
+        @test length(agent_name) > 0
 
-    @test_broken "MD-indexing"
-    @test_broken "Broadcast"
+        agent_isa = get_first_isa(agent)
+        @test length(agent_isa) > 0
+
+        @info "Testing with agent $agent_name with ISA $agent_isa"
+
+        arr = HSAArray(agent, UInt32, (8, ))
+        fill!(arr, UInt32(1))
+        _arr = Array(arr)
+        @test length(arr) == length(_arr)
+        for idx in 1:length(arr)
+            _arr[idx] = arr[idx]
+        end
+        @test arr == _arr
+        fill!(_arr, UInt32(2))
+        for idx in 1:length(arr)
+            arr[idx] = _arr[idx]
+        end
+        @test arr == _arr
+
+        arr1 = HSAArray(agent, UInt32, (8,))
+        arr2 = HSAArray(agent, UInt32, (8,))
+        fill!(arr1, 1)
+        fill!(arr2, 2)
+        arr3 = similar(arr1)
+        for i in 1:length(arr1)
+            arr3[i] = arr1[i] + arr2[i]
+        end
+        @test all(arr3 .== 3)
+        @test all(isa.((arr1, arr2, arr3), Ref(HSAArray)))
+        @test size(arr3) == size(arr1)
+
+        @test_broken "MD-indexing"
+        @test_broken "Broadcast"
+    else
+        @warn("No devices detected; skipping on-device tests")
+    end
 else
     @warn("HSARuntime.jl has not been configured; skipping on-device tests.")
 end
