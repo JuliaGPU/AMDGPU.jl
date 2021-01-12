6435b9fedb0fb5569050b3c01875f8bf55f06f4f
Julian P Samaroo <jpsamaroo@jpsamaroo.me>
2021-01-12 10:47:41 -0600

    Add Documenter deploy

--- docs/make.jl
+++ docs/make.jl
@@ -1,19 +1,33 @@
 using Documenter, AMDGPU
 
-makedocs(
-    sitename="AMDGPU.jl",
-    pages = [
-        "Home" => "index.md",
-        "Quick Start" => "quickstart.md",
-        "Kernel Dependencies" => "kernel_deps.md",
-        "Global Variables" => "globals.md",
-        "Exceptions" => "exceptions.md",
-        "Memory" => "memory.md",
-        "Intrinsics" => [
-            "Execution Control" => "execution_control.md",
-            "Wavefront Operations" => "wavefront_ops.md",
-        ],
-        "API Reference" => "api.md"
-    ]
-)
 
+function main()
+    ci = get(ENV, "CI", "") == "true"
+
+    @info "Generating Documenter site"
+    makedocs(
+        sitename="AMDGPU.jl",
+        pages = [
+            "Home" => "index.md",
+            "Quick Start" => "quickstart.md",
+            "Kernel Dependencies" => "kernel_deps.md",
+            "Global Variables" => "globals.md",
+            "Exceptions" => "exceptions.md",
+            "Memory" => "memory.md",
+            "Intrinsics" => [
+                "Execution Control" => "execution_control.md",
+                "Wavefront Operations" => "wavefront_ops.md",
+            ],
+            "API Reference" => "api.md"
+        ]
+    )
+    if ci
+        @info "Deploying to GitHub"
+        deploydocs(
+            repo = "github.com/JuliaGPU/AMDGPU.jl.git",
+            push_preview = true
+        )
+    end
+end
+
+isinteractive() || main()
