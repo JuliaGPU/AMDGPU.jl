c45b544db07a4ee23941bd383f6b79df889008b1
Takafumi Arakaki <aka.tkf@gmail.com>
2021-04-07 22:56:48 -0400

    More tests

--- test/rocarray/base.jl
+++ test/rocarray/base.jl
@@ -1,7 +1,7 @@
 @testset "Base" begin
 
 @testset "view" begin
-    xs = copy!(ROCVector{Int}(undef, 4), 1:4)
+    xs = copyto!(ROCVector{Int}(undef, 4), 1, collect(1:4), 1, 4)
     a = view(xs, 1:2)
     b = view(xs, 3:4)
     @test a isa ROCVector{Int}
@@ -10,6 +10,14 @@
     @test collect(b)::Vector{Int} == 3:4
     @test a[[1, 2]] == 1:2
     @test b[[1, 2]] == 3:4
+
+    b_cpu = Vector{Int}(undef, 2)
+    @test copyto!(b_cpu, 1, b, 1, 2) === b_cpu
+    @test b_cpu == 3:4
+
+    c = ROCVector{Int}(undef, 2)
+    @test copyto!(c, 1, b, 1, 2) === c
+    @test collect(c) == 3:4
 end
 
 end
