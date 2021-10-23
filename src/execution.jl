28609b59ab3c559ef3b01220ae1fb270cf2ba60e
Valentin Churavy <v.churavy@gmail.com>
2021-10-23 15:51:17 -0400

    Specialize Dataype for broadcasting

--- src/execution.jl
+++ src/execution.jl
@@ -12,6 +12,12 @@ Base.getindex(r::ROCRefValue) = r.x
 Base.setindex!(r::ROCRefValue{T}, value::T) where T = (r.x = value;)
 Adapt.adapt_structure(to::Adaptor, r::Base.RefValue) = ROCRefValue(adapt(to, r[]))
 
+# broadcast sometimes passes a ref(type), resulting in a GPU-incompatible DataType box.
+# avoid that by using a special kind of ref that knows about the boxed type.
+struct ROCRefType{T} <: Ref{DataType} end
+Base.getindex(r::ROCRefType{T}) where T = T
+Adapt.adapt_structure(to::Adaptor, r::Base.RefValue{<:Union{DataType,Type}}) = ROCRefType{r[]}()
+
 """
     rocconvert(x)
 
