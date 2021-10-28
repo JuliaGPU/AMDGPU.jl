b91b95bb551f82b81da058b1dc78c90687168a70
Valentin Churavy <v.churavy@gmail.com>
2021-10-28 17:45:00 -0400

    add more pow-pow

--- src/compat.jl
+++ src/compat.jl
@@ -6,14 +6,6 @@ function LinearAlgebra.norm(v::ROCArray{T}, p::Real=2) where {T}
     elseif p == -Inf
         minimum(abs.(v))
     else
-        # FIXME: AMDGPU.pow(x, p) only support Int32 p
-        if p isa Signed
-            p = Int32(p)
-        elseif p isa Unsigned
-            p = UInt32(p)
-        end
-        let p = p
-            mapreduce(x->AMDGPU.pow(AMDGPU.abs(x), p), +, v; init=zero(T))^(1/p)
-        end
+        mapreduce(x->AMDGPU.pow(AMDGPU.abs(x), p), +, v; init=zero(T))^(1/p)
     end
 end
