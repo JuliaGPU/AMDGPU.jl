9d9e62d67860acc29277eeba6b9cb8ea72e9d218
Ludovic Raess <ludovic.rass@gmail.com>
2022-04-12 09:11:50 +0200

    Add another version

--- src/memory.jl
+++ src/memory.jl
@@ -395,7 +395,7 @@ function unsafe_copy3d!(dst::Ptr{T}, src::Ptr{T}, width, height=1, depth=1;
 
     # println("One needs something here")
     # Libc.systemsleep(0.000001); yield()
-    1==2 && println("no reason to print")
+    # 1==2 && println("no reason to print")
 
     AMDGPU.HSA.amd_memory_async_copy_rect(Base.unsafe_convert(Ptr{HSA.PitchedPtr}, dstRef),
                                           Base.unsafe_convert(Ptr{HSA.Dim3},       dstOffsetRef),
