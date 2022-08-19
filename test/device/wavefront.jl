c68ee24e7f7fc24486d8ad9025d44fcabb507182
Torrance Hodgson <torrance123@gmail.com>
2022-08-19 02:00:35 +0000

    Wrap wavefront info into a common function

--- test/device/wavefront.jl
+++ test/device/wavefront.jl
@@ -1,10 +1,5 @@
 @testset "Wavefront Operations" begin
-    wavefrontsize = Ref{UInt32}()
-    AMDGPU.HSA.LibHSARuntime.hsa_agent_get_info(
-        AMDGPU.default_device().agent,
-        AMDGPU.HSA.AGENT_INFO_WAVEFRONT_SIZE,
-        wavefrontsize
-    )
+    wavefrontsize = AMDGPU.Runtime.wavefront_size(AMDGPU.default_device())
 
     function reduce_kernel(op,X,Y)
         idx = workitemIdx().x
@@ -25,35 +20,35 @@
     end
 
     for T in (Cint, Clong, Cuint, Culong)
-        X = rand(T(1):T(100), wavefrontsize[])
+        X = rand(T(1):T(100), wavefrontsize)
         for op in (Base.:+, max, min, Base.:&, Base.:|, Base.:⊻)
             RX, RY = ROCArray(X), ROCArray(zeros(T,1))
-            wait(@roc groupsize=wavefrontsize[] reduce_kernel(op,RX,RY))
+            wait(@roc groupsize=wavefrontsize reduce_kernel(op,RX,RY))
             @test Array(RY)[1] == reduce(op,X)
 
-            RX, RY = ROCArray(X), ROCArray(zeros(T,wavefrontsize[]))
-            wait(@roc groupsize=wavefrontsize[] scan_kernel(op,RX,RY))
+            RX, RY = ROCArray(X), ROCArray(zeros(T,wavefrontsize))
+            wait(@roc groupsize=wavefrontsize scan_kernel(op,RX,RY))
             @test Array(RY) == accumulate(op,X)
         end
     end
     for T in (Float32, Float64)
-        X = rand(T, wavefrontsize[])
+        X = rand(T, wavefrontsize)
         for op in (Base.:+, max, min)
             RX, RY = ROCArray(X), ROCArray(zeros(T,1))
-            wait(@roc groupsize=wavefrontsize[] reduce_kernel(op,RX,RY))
+            wait(@roc groupsize=wavefrontsize reduce_kernel(op,RX,RY))
             @test Array(RY)[1] ≈ reduce(op,X)
 
-            RX, RY = ROCArray(X), ROCArray(zeros(T,wavefrontsize[]))
-            wait(@roc groupsize=wavefrontsize[] scan_kernel(op,RX,RY))
+            RX, RY = ROCArray(X), ROCArray(zeros(T,wavefrontsize))
+            wait(@roc groupsize=wavefrontsize scan_kernel(op,RX,RY))
             @test Array(RY) ≈ accumulate(op,X)
         end
     end
-    for X in (rand(Cint(0):Cint(1), wavefrontsize[]),
-              zeros(Cint, wavefrontsize[]),
-              ones(Cint, wavefrontsize[]),
+    for X in (rand(Cint(0):Cint(1), wavefrontsize),
+              zeros(Cint, wavefrontsize),
+              ones(Cint, wavefrontsize),
               )
         RX, RY = ROCArray(X), ROCArray(zeros(Bool,3))
-        wait(@roc groupsize=wavefrontsize[] bool_kernel(RX,RY))
+        wait(@roc groupsize=wavefrontsize bool_kernel(RX,RY))
         Y = Array(RY)
         @test_skip Y[1] == all(x->x==1,X)
         @test_skip Y[2] == any(x->x==1,X)
@@ -62,14 +57,9 @@
 end
 
 @testset "Wavefront Information" begin
-    wavefrontsize = Ref{UInt32}()
-    AMDGPU.HSA.LibHSARuntime.hsa_agent_get_info(
-        AMDGPU.default_device().agent,
-        AMDGPU.HSA.AGENT_INFO_WAVEFRONT_SIZE,
-        wavefrontsize
-    )
+    wavefrontsize = AMDGPU.Runtime.wavefront_size(AMDGPU.default_device())
 
-    @test wavefrontsize[] == 32 || wavefrontsize[] == 64
+    @test wavefrontsize == 32 || wavefrontsize == 64
 
     function kernel(X)
         X[1] = Device.wavefrontsize()
@@ -77,5 +67,5 @@ end
     end
     RX = ROCArray(zeros(UInt32, 1))
     wait(@roc kernel(RX))
-    @allowscalar @test RX[1] == wavefrontsize[]
+    @allowscalar @test RX[1] == wavefrontsize
 end
