2c969649061d314582b6be4e56a59a7148dd562c
matinraayai <raayaiardani.m@northeastern.edu>
2022-08-30 15:26:00 -0400

    Added util.jl from CUSPARSE.
    (cherry picked from commit 62cf82dd556501d5405e9b02ebc0e9875b3cf612)

---
+++ src/sparse/util.jl
@@ -0,0 +1,23 @@
+# utility functions for the ROCSPARSE wrappers
+
+"""
+check that the dimensions of matrix `X` and vector `Y` make sense for a multiplication
+"""
+function chkmvdims(X, n, Y, m)
+    if length(X) != n
+        throw(DimensionMismatch("X must have length $n, but has length $(length(X))"))
+    elseif length(Y) != m
+        throw(DimensionMismatch("Y must have length $m, but has length $(length(Y))"))
+    end
+end
+
+"""
+check that the dimensions of matrices `X` and `Y` make sense for a multiplication
+"""
+function chkmmdims( B, C, k, l, m, n )
+    if size(B) != (k,l)
+        throw(DimensionMismatch("B has dimensions $(size(B)) but needs ($k,$l)"))
+    elseif size(C) != (m,n)
+        throw(DimensionMismatch("C has dimensions $(size(C)) but needs ($m,$n)"))
+    end
+end
