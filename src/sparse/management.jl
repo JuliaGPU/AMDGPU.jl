183cadb4a36c1c986cfb50c60436a14ee7767e85
matinraayai <raayaiardani.m@northeastern.edu>
2022-08-31 19:07:17 -0400

    added management.jl for rocsparse.
    (cherry picked from commit b62e2653c9563466b0e2e32eada4d5bb9da0b995)

---
+++ src/sparse/management.jl
@@ -0,0 +1,7 @@
+# rocSPARSE functions for managing the library
+
+function rocsparse_create()
+    handle = Ref{rocsparse_create_handle}()
+    @check unsafe_rocsparse_create_handle(handle) rocsparse_status_not_initialized
+    handle[]
+end
