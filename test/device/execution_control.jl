5ca4d4adc46a0dead94c0c3e1376096d5b07a65f
Julian P Samaroo <jpsamaroo@jpsamaroo.me>
2022-11-08 12:13:18 -0600

    Add memtime and memrealtime intrinsics

--- test/device/execution_control.jl
+++ test/device/execution_control.jl
@@ -24,4 +24,20 @@ str = String(take!(iob))
 @test occursin("s_sendmsghalt ", str)
 @test occursin("s_endpgm", str) # TODO: Test for multiple occurences
 
+@testset "device_sleep/memtime/memrealtime" begin
+    function time_kernel(X)
+        t1 = AMDGPU.Device.memtime()
+        tr1 = AMDGPU.Device.memrealtime()
+        AMDGPU.Device.device_sleep(Int32(1))
+        t2 = AMDGPU.Device.memtime()
+        tr2 = AMDGPU.Device.memrealtime()
+        X[1] = t2 > t1
+        X[2] = tr2 > tr1
+        return
+    end
+    RX = ROCArray(zeros(Bool, 2))
+    wait(@roc time_kernel(RX))
+    @test all(Array(RX))
+end
+
 end
