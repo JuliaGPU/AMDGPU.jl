692b40015a200e9ab53c75df65c7903899dc302c
Julian P Samaroo <jpsamaroo@jpsamaroo.me>
2022-11-15 19:46:31 -0600

    compiler: Reorganization

--- src/AMDGPU.jl
+++ src/AMDGPU.jl
@@ -144,9 +144,10 @@ module Compiler
     import .Runtime: Adaptor
     import .Runtime: Mem
 
-    include("devlibs.jl")
-    include("compiler_utils.jl")
-    include("compiler.jl")
+    include("compiler/device-libs.jl")
+    include("compiler/utils.jl")
+    include("compiler/global-hooks.jl")
+    include("compiler/codegen.jl")
 end # module Compiler
 
 include("highlevel.jl")
