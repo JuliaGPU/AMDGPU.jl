62b1e3c7ef4a6436925412bdea8dfebca8b5f133
Julian P Samaroo <jpsamaroo@jpsamaroo.me>
2022-12-09 07:44:38 -0600

    intrinsics: Add readfirstlane

--- test/device/execution_control.jl
+++ test/device/execution_control.jl
@@ -1,28 +1,33 @@
 @testset "Execution Control Intrinsics" begin
 
-function completion_signal_kernel(X)
-    X[1] = Device._completion_signal()
-    nothing
+@testset "Completion Signal" begin
+    function completion_signal_kernel(X)
+        X[1] = AMDGPU.Device._completion_signal()
+        nothing
+    end
+
+    RA = ROCArray(rand(UInt64, 1))
+
+    ev = @roc completion_signal_kernel(RA)
+    wait(ev)
+    @test Array(RA)[1] == ev.signal.signal[].handle
 end
 
-RA = ROCArray(rand(UInt64, 1))
+@testset "sendmsg/sendmsghalt/endpgm" begin
+    function exec_ctl_kernel()
+        Device.sendmsg(5)
+        Device.sendmsghalt(6)
+        Device.endpgm()
+    end
 
-ev = @roc completion_signal_kernel(RA)
-wait(ev)
-@test collect(RA)[1] == ev.signal.signal[].handle
-
-function exec_ctl_kernel(X)
-    Device.sendmsg(5)
-    Device.sendmsghalt(6)
-    Device.endpgm()
+    iob = IOBuffer()
+    AMDGPU.code_native(iob, exec_ctl_kernel, Tuple{})
+    str = String(take!(iob))
+    @test occursin("s_sendmsg ", str)
+    @test occursin("s_sendmsghalt ", str)
+    # TODO: Can't easily count these, since they're automatically inserted
+    @test occursin("s_endpgm", str)
 end
-
-iob = IOBuffer()
-AMDGPU.code_native(iob, exec_ctl_kernel, Tuple{Int64}; kernel=true)
-str = String(take!(iob))
-@test occursin("s_sendmsg ", str)
-@test occursin("s_sendmsghalt ", str)
-@test occursin("s_endpgm", str) # TODO: Test for multiple occurences
 
 @testset "device_sleep/memtime/memrealtime" begin
     function time_kernel(X)
@@ -38,6 +43,24 @@ str = String(take!(iob))
     RX = ROCArray(zeros(Bool, 2))
     wait(@roc time_kernel(RX))
     @test all(Array(RX))
+end
+
+@testset "readfirstlane" begin
+    function readfirstlane_kernel(B, A)
+        idx = workitemIdx().x
+        if idx > 1
+            B[idx] = AMDGPU.Device.readfirstlane(A[idx])
+        else
+            B[idx] = A[idx]
+        end
+        return
+    end
+    RB = ROCArray(zeros(Int32, 8))
+    RA = ROCArray(Int32(1):Int32(8))
+    wait(@roc groupsize=8 readfirstlane_kernel(RB, RA))
+    B = Array(RB)
+    @test B[1] == Int32(1)
+    @test all(B[2:8] .== Int32(2))
 end
 
 end
