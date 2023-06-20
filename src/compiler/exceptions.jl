d78ed6dd014f03c4549de70a8ff69c4275488484
Anton Smirnov <Anton.Smirnov@unil.ch>
2023-06-20 16:16:15 +0300

    Store device in HIPModule

--- src/compiler/exceptions.jl
+++ src/compiler/exceptions.jl
@@ -1,15 +1,15 @@
-struct KernelException <: Exception end
+struct KernelException <: Exception
+    dev::HIP.HIPDevice
+end
 
-# TODO report what device.
 function Base.showerror(io::IO, ex::KernelException)
-    print(io, "KernelException: exception thrown during kernel execution on device.")
+    print(io, "KernelException: exception thrown during kernel execution on `$(ex.dev)`.")
 end
 
 const _exception_flags = Dict{HIP.HIPDevice, Mem.HostBuffer}()
 
 function create_exception!(mod::HIP.HIPModule)
-    dev = HIP.device() # TODO store device in mod.
-    exception_flag = get!(_exception_flags, dev,
+    exception_flag = get!(_exception_flags, mod.dev,
         Mem.HostBuffer(sizeof(Int), HIP.hipHostAllocMapped))
     return Mem.device_ptr(exception_flag)
 end
@@ -21,7 +21,7 @@ function check_exceptions()
         flag = unsafe_load(ptr)
         if flag != 0
             unsafe_store!(ptr, 0)
-            throw(KernelException()) # TODO pass device
+            throw(KernelException(dev))
         end
     end
 end
