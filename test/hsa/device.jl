d80306aac0f47909f925b77ce8e30900ba0cf6c7
Anton Smirnov <tonysmn97@gmail.com>
2023-09-06 15:10:38 +0300

    Update tests

--- test/hsa/device.jl
+++ test/hsa/device.jl
@@ -7,9 +7,8 @@
     end
 
     @testset "Default selection" begin
-        device = AMDGPU.default_device()
+        device = AMDGPU.device()
         @test device !== nothing
-        @test device == AMDGPU.device()
 
         device_name = HIP.name(device)
         @test length(device_name) > 0
@@ -17,20 +16,20 @@
 
         if length(AMDGPU.devices()) > 1
             @testset "Multi-GPU" begin
-                init_device = AMDGPU.default_device()
-                init_device_id = AMDGPU.default_device_id()
+                init_device = AMDGPU.device()
+                init_device_id = AMDGPU.device_id()
                 @test init_device_id == 1
 
-                AMDGPU.default_device_id!(2)
-                @test AMDGPU.default_device_id() == 2
-                @test AMDGPU.default_device() != init_device
+                AMDGPU.device_id!(2)
+                @test AMDGPU.device_id() == 2
+                @test AMDGPU.device() != init_device
 
-                AMDGPU.default_device_id!(1)
-                @test AMDGPU.default_device_id() == 1
-                @test AMDGPU.default_device() == init_device
+                AMDGPU.device_id!(1)
+                @test AMDGPU.device_id() == 1
+                @test AMDGPU.device() == init_device
 
-                @test_throws BoundsError AMDGPU.default_device_id!(0)
-                @test_throws BoundsError AMDGPU.default_device_id!(length(AMDGPU.devices())+1)
+                @test_throws BoundsError AMDGPU.device_id!(0)
+                @test_throws BoundsError AMDGPU.device_id!(length(AMDGPU.devices()) + 1)
             end
         else
             @test_skip "Multi-GPU"
@@ -38,7 +37,7 @@
     end
 
     @testset "ISAs" begin
-        device = AMDGPU.default_device()
+        device = AMDGPU.device()
         device_isa, features = AMDGPU.default_isa(device).arch_features
         @test length(device_isa) > 0
         @test occursin("gfx", device_isa)
