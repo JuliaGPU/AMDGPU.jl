584f24f006ecceb670214ed4eb7e898209e2a06b
Alexis Montoison <35051714+amontoison@users.noreply.github.com>
2023-09-07 04:46:19 -0500

    [rosSPARSE] Support matrix-vector products with COO format (#488)

--- src/sparse/generic.jl
+++ src/sparse/generic.jl
@@ -60,7 +60,7 @@ mutable struct ROCSparseMatrixDescriptor
     function ROCSparseMatrixDescriptor(A::ROCSparseMatrixCSR, IndexBase::Char)
         desc_ref = Ref{rocsparse_spmat_descr}()
         rocsparse_create_csr_descr(
-            desc_ref, size(A)..., length(nonzeros(A)),
+            desc_ref, size(A)..., nnz(A),
             A.rowPtr, A.colVal, nonzeros(A),
             eltype(A.rowPtr), eltype(A.colVal), IndexBase, eltype(nonzeros(A)))
         obj = new(desc_ref[])
@@ -68,20 +68,38 @@ mutable struct ROCSparseMatrixDescriptor
         return obj
     end
 
-    function ROCSparseMatrixDescriptor(A::ROCSparseMatrixCSC, IndexBase::Char; convert=true)
+    function ROCSparseMatrixDescriptor(A::ROCSparseMatrixCSC, IndexBase::Char; transposed::Bool=false)
         desc_ref = Ref{rocsparse_spmat_descr}()
-        if convert
-            # many algorithms, e.g. mv! and mm!, do not support CSC sparse format
-            # so we eagerly convert this to a CSR matrix.
+        if transposed
             rocsparse_create_csr_descr(
-                desc_ref, reverse(size(A))..., length(nonzeros(A)),
+                desc_ref, reverse(size(A))..., nnz(A),
                 A.colPtr, rowvals(A), nonzeros(A),
                 eltype(A.colPtr), eltype(rowvals(A)), IndexBase, eltype(nonzeros(A)))
         else
             rocsparse_create_csc_descr(
-                desc_ref, size(A)..., length(nonzeros(A)),
+                desc_ref, size(A)..., nnz(A),
                 A.colPtr, rowvals(A), nonzeros(A),
                 eltype(A.colPtr), eltype(rowvals(A)), IndexBase, eltype(nonzeros(A)))
+        end
+        obj = new(desc_ref[])
+        finalizer(rocsparse_destroy_spmat_descr, obj)
+        return obj
+    end
+
+    function ROCSparseMatrixDescriptor(A::ROCSparseMatrixCOO, IndexBase::Char; transposed::Bool=false)
+        desc_ref = Ref{rocsparse_spmat_descr}()
+        if transposed
+            rocsparse_create_coo_descr(
+                desc_ref, reverse(size(A))..., nnz(A),
+                A.colInd, A.rowInd, nonzeros(A),
+                eltype(A.colInd), IndexBase, eltype(nonzeros(A))
+            )
+        else
+            rocsparse_create_coo_descr(
+                desc_ref, size(A)..., nnz(A),
+                A.rowInd, A.colInd, nonzeros(A),
+                eltype(A.rowInd), IndexBase, eltype(nonzeros(A))
+            )
         end
         obj = new(desc_ref[])
         finalizer(rocsparse_destroy_spmat_descr, obj)
@@ -101,11 +119,28 @@ function gather!(X::ROCSparseVector, Y::ROCVector, index::SparseChar)
 end
 
 function mv!(
-    transa::SparseChar, alpha::Number, A::Union{ROCSparseMatrixBSR{TA}, ROCSparseMatrixCSR{TA}},
+    transa::SparseChar, alpha::Number, A::Union{ROCSparseMatrixCSR{TA}, ROCSparseMatrixCSC{TA}, ROCSparseMatrixCOO{TA}},
     X::DenseROCVector{T}, beta::Number, Y::DenseROCVector{T}, index::SparseChar,
     algo::rocsparse_spmv_alg = rocsparse_spmv_alg_default,
 ) where {TA, T}
-    m,n = size(A)
+
+    # Support transa = 'C' for real matrices
+    transa = T <: Real && transa == 'C' ? 'T' : transa
+
+    if isa(A, ROCSparseMatrixCSC) && transa == 'C' && TA <: Complex
+        throw(ArgumentError(
+            "Matrix-vector multiplication with the adjoint of a CSC matrix" *
+            " is not supported. Use a CSR or COO matrix instead."))
+    end
+
+    if isa(A, ROCSparseMatrixCSC)
+        descA = ROCSparseMatrixDescriptor(A, index, transposed=true)
+        n,m = size(A)
+        transa = transa == 'N' ? 'T' : 'N'
+    else
+        descA = ROCSparseMatrixDescriptor(A, index)
+        m,n = size(A)
+    end
 
     if transa == 'N'
         chkmvdims(X,n,Y,m)
@@ -113,7 +148,6 @@ function mv!(
         chkmvdims(X,m,Y,n)
     end
 
-    descA = ROCSparseMatrixDescriptor(A, index)
     descX = ROCDenseVectorDescriptor(X)
     descY = ROCDenseVectorDescriptor(Y)
 
@@ -146,67 +180,30 @@ function mv!(
     Y
 end
 
-function mv!(
-    transa::SparseChar, alpha::Number, A::ROCSparseMatrixCSC{TA}, X::DenseROCVector{T},
-    beta::Number, Y::DenseROCVector{T}, index::SparseChar,
-    algo::rocsparse_spmv_alg = rocsparse_spmv_alg_default,
-) where {TA, T}
-    ctransa = 'N'
-    if transa == 'N'
-        ctransa = 'T'
-    elseif transa == 'C' && TA <: Complex
-        throw(ArgumentError(
-            "Matrix-vector multiplication with the adjoint of a CSC matrix" *
-            " is not supported. Use a CSR matrix instead."))
-    end
-
-    n, m = size(A)
-
-    if ctransa == 'N'
-        chkmvdims(X,n,Y,m)
-    elseif ctransa == 'T' || ctransa == 'C'
-        chkmvdims(X,m,Y,n)
-    end
-
-    descA = ROCSparseMatrixDescriptor(A, index)
-    descX = ROCDenseVectorDescriptor(X)
-    descY = ROCDenseVectorDescriptor(Y)
-
-    # operations with 16-bit numbers always imply mixed-precision computation
-    # TODO: we should better model the supported combinations here,
-    #       and error if using an unsupported one (like with gemmEx!)
-    compute_type = if T == Float16
-        Float32
-    elseif T == ComplexF16
-        ComplexF32
-    else
-        T
-    end
-
-    function bufferSize()
-        out = Ref{Csize_t}()
-        rocsparse_spmv(
-            handle(), ctransa, Ref{compute_type}(alpha), descA, descX,
-            Ref{compute_type}(beta), descY, compute_type, algo, out, C_NULL)
-        return out[]
-    end
-
-    buff_size = Ref{Csize_t}()
-    with_workspace(bufferSize) do buffer
-        buff_size[] = sizeof(buffer)
-        rocsparse_spmv(
-            handle(), ctransa, Ref{compute_type}(alpha), descA, descX,
-            Ref{compute_type}(beta), descY, compute_type, algo, buff_size, buffer)
-    end
-    return Y
-end
-
 function mm!(
-    transa::SparseChar, transb::SparseChar, alpha::Number, A::ROCSparseMatrixCSR{T},
+    transa::SparseChar, transb::SparseChar, alpha::Number, A::Union{ROCSparseMatrixCSR{T}, ROCSparseMatrixCSC{T}, ROCSparseMatrixCOO{T}},
     B::DenseROCMatrix{T}, beta::Number, C::DenseROCMatrix{T}, index::SparseChar,
     algo::rocsparse_spmm_alg=rocsparse_spmm_alg_default,
 ) where T
-    m, k = size(A)
+
+    # Support transa = 'C' and `transb = 'C' for real matrices
+    transa = T <: Real && transa == 'C' ? 'T' : transa
+    transb = T <: Real && transb == 'C' ? 'T' : transb
+
+    if isa(A, ROCSparseMatrixCSC) && transa == 'C' && T <: Complex
+        throw(ArgumentError(
+            "Matrix-matrix multiplication with the adjoint of a CSC matrix" *
+            " is not supported. Use a CSR or COO matrix instead."))
+    end
+
+    if isa(A, ROCSparseMatrixCSC)
+        descA = ROCSparseMatrixDescriptor(A, index, transposed=true)
+        k,m = size(A)
+        transa = transa == 'N' ? 'T' : 'N'
+    else
+        descA = ROCSparseMatrixDescriptor(A, index)
+        m,k = size(A)
+    end
     n = size(C)[2]
 
     if transa == 'N' && transb == 'N'
@@ -219,7 +216,6 @@ function mm!(
         chkmmdims(B,C,n,m,k,n)
     end
 
-    descA = ROCSparseMatrixDescriptor(A, index)
     descB = ROCDenseMatrixDescriptor(B)
     descC = ROCDenseMatrixDescriptor(C)
 
@@ -238,57 +234,6 @@ function mm!(
             descC, T, algo, rocsparse_spmm_stage_preprocess, buffer_len_ref, buffer)
         rocsparse_spmm(
             handle(), transa, transb, Ref{T}(alpha), descA, descB, Ref{T}(beta),
-            descC, T, algo, rocsparse_spmm_stage_compute, buffer_len_ref, buffer)
-    end
-    return C
-end
-
-function mm!(
-    transa::SparseChar, transb::SparseChar, alpha::Number, A::ROCSparseMatrixCSC{T},
-    B::DenseROCMatrix{T}, beta::Number, C::DenseROCMatrix{T}, index::SparseChar,
-    algo::rocsparse_spmm_alg = rocsparse_spmm_alg_default,
-) where T
-    ctransa = 'N'
-    if transa == 'N'
-        ctransa = 'T'
-    elseif transa == 'C' && T <: Complex
-        throw(ArgumentError(
-            "Matrix-matrix multiplication with the adjoint of a CSC matrix" *
-            " is not supported. Use a CSR matrix instead."))
-    end
-
-    k, m = size(A)
-    n = size(C)[2]
-
-    if ctransa == 'N' && transb == 'N'
-        chkmmdims(B,C,k,n,m,n)
-    elseif ctransa == 'N' && transb != 'N'
-        chkmmdims(B,C,n,k,m,n)
-    elseif ctransa != 'N' && transb == 'N'
-        chkmmdims(B,C,m,n,k,n)
-    elseif ctransa != 'N' && transb != 'N'
-        chkmmdims(B,C,n,m,k,n)
-    end
-
-    descA = ROCSparseMatrixDescriptor(A, index)
-    descB = ROCDenseMatrixDescriptor(B)
-    descC = ROCDenseMatrixDescriptor(C)
-
-    function bufferSize()
-        out = Ref{Csize_t}()
-        rocsparse_spmm(
-            handle(), ctransa, transb, Ref{T}(alpha), descA, descB, Ref{T}(beta),
-            descC, T, algo, rocsparse_spmm_stage_buffer_size, out, C_NULL)
-        return out[]
-    end
-
-    with_workspace(bufferSize) do buffer
-        buffer_len_ref = Ref{Csize_t}(sizeof(buffer))
-        rocsparse_spmm(
-            handle(), ctransa, transb, Ref{T}(alpha), descA, descB, Ref{T}(beta),
-            descC, T, algo, rocsparse_spmm_stage_preprocess, buffer_len_ref, buffer)
-        rocsparse_spmm(
-            handle(), ctransa, transb, Ref{T}(alpha), descA, descB, Ref{T}(beta),
             descC, T, algo, rocsparse_spmm_stage_compute, buffer_len_ref, buffer)
     end
     return C
--- test/rocsparse/generic.jl
+++ test/rocsparse/generic.jl
@@ -35,22 +35,44 @@ end
     end
 end
 
-@testset "mv algo=$algo" for algo in (
-    rocSPARSE.rocsparse_spmv_alg_default,
-    # rocSPARSE.rocsparse_spmv_alg_coo, # No COO or ELL matrix types
-    # rocSPARSE.rocsparse_spmv_alg_ell,
-)
-    @testset "mv $T" for T in (Float32, Float64, ComplexF32, ComplexF64)
-        A = sprand(T, 10, 10, 0.1)
-        B = rand(T, 10)
-        C = rand(T, 10)
-        dA = ROCSparseMatrixCSR(A)
-        dB = ROCArray(B)
-        dC = ROCArray(C)
+for SparseMatrixType in (ROCSparseMatrixCSR, ROCSparseMatrixCSC, ROCSparseMatrixCOO)
 
-        alpha = 1.2
-        beta = 1.3
-        mv!('N', alpha, dA, dB, beta, dC, 'O', algo)
-        @test alpha * A * B + beta * C ≈ collect(dC)
+    @testset "$SparseMatrixType -- mv! algo=$algo" for algo in (rocSPARSE.rocsparse_spmv_alg_default,)
+        @testset "mv! $T" for T in [Float32, Float64, ComplexF32, ComplexF64]
+            for (transa, opa) in [('N', identity), ('T', transpose), ('C', adjoint)]
+                SparseMatrixType == ROCSparseMatrixCSC && T <: Complex && transa == 'C' && continue
+                A = sprand(T, 20, 10, 0.1)
+                B = transa == 'N' ? rand(T, 10) : rand(T, 20)
+                C = transa == 'N' ? rand(T, 20) : rand(T, 10)
+                dA = SparseMatrixType(A)
+                dB = ROCArray(B)
+                dC = ROCArray(C)
+
+                alpha = rand(T)
+                beta = rand(T)
+                mv!(transa, alpha, dA, dB, beta, dC, 'O', algo)
+                @test alpha * opa(A) * B + beta * C ≈ collect(dC)
+            end
+        end
+    end
+
+    @testset "$SparseMatrixType -- mm! algo=$algo" for algo in (rocSPARSE.rocsparse_spmm_alg_default,)
+        @testset "mm! $T" for T in [Float32, Float64, ComplexF32, ComplexF64]
+            for (transa, opa) in [('N', identity), ('T', transpose), ('C', adjoint)]
+                for (transb, opb) in [('N', identity), ('T', transpose), ('C', adjoint)]
+                    SparseMatrixType == ROCSparseMatrixCSC && T <: Complex && transa == 'C' && continue
+                    SparseMatrixType == ROCSparseMatrixCOO && transa != 'N' && continue
+                    A = sprand(T, 10, 10, 0.1)
+                    B = transb == 'N' ? rand(T, 10, 2) : rand(T, 2, 10)
+                    C = rand(T, 10, 2)
+                    dA = SparseMatrixType(A)
+                    dB, dC = ROCArray(B), ROCArray(C)
+
+                    alpha, beta = rand(T), rand(T)
+                    mm!(transa, transb, alpha, dA, dB, beta, dC, 'O', algo)
+                    @test alpha * opa(A) * opb(B) + beta * C ≈ collect(dC)
+                end
+            end
+        end
     end
 end
