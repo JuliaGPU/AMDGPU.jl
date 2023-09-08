2d6f1e4e8d7ea44ab4067ae2e4d0e5ba1d2ce9cf
Anton Smirnov <tonysmn97@gmail.com>
2023-09-08 13:32:09 +0300

    Prepare state before API calls

--- src/fft/librocfft.jl
+++ src/fft/librocfft.jl
@@ -62,75 +62,93 @@ const rocfft_array_type = rocfft_array_type_e
 
 # no prototype is found for this function at rocfft.h:124:29, please use with caution
 function rocfft_setup()
+    AMDGPU.prepare_state()
     ccall((:rocfft_setup, librocfft), rocfft_status, ()) |> check
 end
 
 # no prototype is found for this function at rocfft.h:128:29, please use with caution
 function rocfft_cleanup()
+    AMDGPU.prepare_state()
     ccall((:rocfft_cleanup, librocfft), rocfft_status, ()) |> check
 end
 
 function rocfft_plan_create(plan, placement, transform_type, precision, dimensions, lengths, number_of_transforms, description)
+    AMDGPU.prepare_state()
     ccall((:rocfft_plan_create, librocfft), rocfft_status, (Ptr{rocfft_plan}, rocfft_result_placement, rocfft_transform_type, rocfft_precision, Cint, Ptr{Cint}, Cint, rocfft_plan_description), plan, placement, transform_type, precision, dimensions, lengths, number_of_transforms, description) |> check
 end
 
 function rocfft_execute(plan, in_buffer, out_buffer, info)
+    AMDGPU.prepare_state()
     ccall((:rocfft_execute, librocfft), rocfft_status, (rocfft_plan, Ptr{Ptr{Cvoid}}, Ptr{Ptr{Cvoid}}, rocfft_execution_info), plan, in_buffer, out_buffer, info) |> check
 end
 
 function rocfft_plan_destroy(plan)
+    AMDGPU.prepare_state()
     ccall((:rocfft_plan_destroy, librocfft), rocfft_status, (rocfft_plan,), plan) |> check
 end
 
 function rocfft_plan_description_set_scale_factor(description, scale_factor)
+    AMDGPU.prepare_state()
     ccall((:rocfft_plan_description_set_scale_factor, librocfft), rocfft_status, (rocfft_plan_description, Cdouble), description, scale_factor) |> check
 end
 
 function rocfft_plan_description_set_data_layout(description, in_array_type, out_array_type, in_offsets, out_offsets, in_strides_size, in_strides, in_distance, out_strides_size, out_strides, out_distance)
+    AMDGPU.prepare_state()
     ccall((:rocfft_plan_description_set_data_layout, librocfft), rocfft_status, (rocfft_plan_description, rocfft_array_type, rocfft_array_type, Ptr{Cint}, Ptr{Cint}, Cint, Ptr{Cint}, Cint, Cint, Ptr{Cint}, Cint), description, in_array_type, out_array_type, in_offsets, out_offsets, in_strides_size, in_strides, in_distance, out_strides_size, out_strides, out_distance) |> check
 end
 
 function rocfft_get_version_string(buf, len)
+    AMDGPU.prepare_state()
     ccall((:rocfft_get_version_string, librocfft), rocfft_status, (Ptr{Cchar}, Cint), buf, len) |> check
 end
 
 function rocfft_plan_get_work_buffer_size(plan, size_in_bytes)
+    AMDGPU.prepare_state()
     ccall((:rocfft_plan_get_work_buffer_size, librocfft), rocfft_status, (rocfft_plan, Ptr{Cint}), plan, size_in_bytes) |> check
 end
 
 function rocfft_plan_get_print(plan)
+    AMDGPU.prepare_state()
     ccall((:rocfft_plan_get_print, librocfft), rocfft_status, (rocfft_plan,), plan) |> check
 end
 
 function rocfft_plan_description_create(description)
+    AMDGPU.prepare_state()
     ccall((:rocfft_plan_description_create, librocfft), rocfft_status, (Ptr{rocfft_plan_description},), description) |> check
 end
 
 function rocfft_plan_description_destroy(description)
+    AMDGPU.prepare_state()
     ccall((:rocfft_plan_description_destroy, librocfft), rocfft_status, (rocfft_plan_description,), description) |> check
 end
 
 function rocfft_execution_info_create(info)
+    AMDGPU.prepare_state()
     ccall((:rocfft_execution_info_create, librocfft), rocfft_status, (Ptr{rocfft_execution_info},), info) |> check
 end
 
 function rocfft_execution_info_destroy(info)
+    AMDGPU.prepare_state()
     ccall((:rocfft_execution_info_destroy, librocfft), rocfft_status, (rocfft_execution_info,), info) |> check
 end
 
 function rocfft_execution_info_set_work_buffer(info, work_buffer, size_in_bytes)
+    AMDGPU.prepare_state()
     ccall((:rocfft_execution_info_set_work_buffer, librocfft), rocfft_status, (rocfft_execution_info, Ptr{Cvoid}, Cint), info, work_buffer, size_in_bytes) |> check
 end
 
 function rocfft_execution_info_set_stream(info, stream)
+    AMDGPU.prepare_state()
     ccall((:rocfft_execution_info_set_stream, librocfft), rocfft_status, (rocfft_execution_info, hipStream_t), info, stream) |> check
 end
 
 function rocfft_execution_info_set_load_callback(info, cb_functions, cb_data, shared_mem_bytes)
+    AMDGPU.prepare_state()
     ccall((:rocfft_execution_info_set_load_callback, librocfft), rocfft_status, (rocfft_execution_info, Ptr{Ptr{Cvoid}}, Ptr{Ptr{Cvoid}}, Cint), info, cb_functions, cb_data, shared_mem_bytes) |> check
 end
 
 function rocfft_execution_info_set_store_callback(info, cb_functions, cb_data, shared_mem_bytes)
+    AMDGPU.prepare_state()
     ccall((:rocfft_execution_info_set_store_callback, librocfft), rocfft_status, (rocfft_execution_info, Ptr{Ptr{Cvoid}}, Ptr{Ptr{Cvoid}}, Cint), info, cb_functions, cb_data, shared_mem_bytes) |> check
 end
 
--- src/rand/librocrand.jl
+++ src/rand/librocrand.jl
@@ -1,154 +1,119 @@
-# Julia wrapper for header: rocrand_version.h
-# Automatically generated using Clang.jl
-
-# Julia wrapper for header: rocrand_discrete_types.h
-# Automatically generated using Clang.jl
-
-# Julia wrapper for header: rocrand.h
-# Automatically generated using Clang.jl
-
-
 function rocrand_create_generator(generator, rng_type)
-    @check ccall((:rocrand_create_generator, librocrand), rocrand_status,
-                 (Ptr{rocrand_generator}, rocrand_rng_type),
-                 generator, rng_type)
+    AMDGPU.prepare_state()
+    @check ccall((:rocrand_create_generator, librocrand), rocrand_status, (Ptr{rocrand_generator}, rocrand_rng_type), generator, rng_type)
 end
 
 function rocrand_destroy_generator(generator)
-    @check ccall((:rocrand_destroy_generator, librocrand), rocrand_status,
-                 (rocrand_generator,),
-                 generator)
+    AMDGPU.prepare_state()
+    @check ccall((:rocrand_destroy_generator, librocrand), rocrand_status, (rocrand_generator,), generator)
 end
 
 function rocrand_generate(generator, output_data, n)
-    @check ccall((:rocrand_generate, librocrand), rocrand_status,
-                 (rocrand_generator, Ptr{UInt32}, Cint),
-                 generator, output_data, n)
+    AMDGPU.prepare_state()
+    @check ccall((:rocrand_generate, librocrand), rocrand_status, (rocrand_generator, Ptr{UInt32}, Cint), generator, output_data, n)
 end
 
 function rocrand_generate_char(generator, output_data, n)
-    @check ccall((:rocrand_generate_char, librocrand), rocrand_status,
-                 (rocrand_generator, Ptr{Cuchar}, Cint),
-                 generator, output_data, n)
+    AMDGPU.prepare_state()
+    @check ccall((:rocrand_generate_char, librocrand), rocrand_status, (rocrand_generator, Ptr{Cuchar}, Cint), generator, output_data, n)
 end
 
 function rocrand_generate_short(generator, output_data, n)
-    @check ccall((:rocrand_generate_short, librocrand), rocrand_status,
-                 (rocrand_generator, Ptr{UInt16}, Cint),
-                 generator, output_data, n)
+    AMDGPU.prepare_state()
+    @check ccall((:rocrand_generate_short, librocrand), rocrand_status, (rocrand_generator, Ptr{UInt16}, Cint), generator, output_data, n)
 end
 
 function rocrand_generate_uniform(generator, output_data, n)
-    @check ccall((:rocrand_generate_uniform, librocrand), rocrand_status,
-                 (rocrand_generator, Ptr{Cfloat}, Cint),
-                 generator, output_data, n)
+    AMDGPU.prepare_state()
+    @check ccall((:rocrand_generate_uniform, librocrand), rocrand_status, (rocrand_generator, Ptr{Cfloat}, Cint), generator, output_data, n)
 end
 
 function rocrand_generate_uniform_double(generator, output_data, n)
-    @check ccall((:rocrand_generate_uniform_double, librocrand), rocrand_status,
-                 (rocrand_generator, Ptr{Cdouble}, Cint),
-                 generator, output_data, n)
+    AMDGPU.prepare_state()
+    @check ccall((:rocrand_generate_uniform_double, librocrand), rocrand_status, (rocrand_generator, Ptr{Cdouble}, Cint), generator, output_data, n)
 end
 
 function rocrand_generate_uniform_half(generator, output_data, n)
-    @check ccall((:rocrand_generate_uniform_half, librocrand), rocrand_status,
-                 (rocrand_generator, Ptr{half}, Cint),
-                 generator, output_data, n)
+    AMDGPU.prepare_state()
+    @check ccall((:rocrand_generate_uniform_half, librocrand), rocrand_status, (rocrand_generator, Ptr{half}, Cint), generator, output_data, n)
 end
 
 function rocrand_generate_normal(generator, output_data, n, mean, stddev)
-    @check ccall((:rocrand_generate_normal, librocrand), rocrand_status,
-                 (rocrand_generator, Ptr{Cfloat}, Cint, Cfloat, Cfloat),
-                 generator, output_data, n, mean, stddev)
+    AMDGPU.prepare_state()
+    @check ccall((:rocrand_generate_normal, librocrand), rocrand_status, (rocrand_generator, Ptr{Cfloat}, Cint, Cfloat, Cfloat), generator, output_data, n, mean, stddev)
 end
 
 function rocrand_generate_normal_double(generator, output_data, n, mean, stddev)
-    @check ccall((:rocrand_generate_normal_double, librocrand), rocrand_status,
-                 (rocrand_generator, Ptr{Cdouble}, Cint, Cdouble, Cdouble),
-                 generator, output_data, n, mean, stddev)
+    AMDGPU.prepare_state()
+    @check ccall((:rocrand_generate_normal_double, librocrand), rocrand_status, (rocrand_generator, Ptr{Cdouble}, Cint, Cdouble, Cdouble), generator, output_data, n, mean, stddev)
 end
 
 function rocrand_generate_normal_half(generator, output_data, n, mean, stddev)
-    @check ccall((:rocrand_generate_normal_half, librocrand), rocrand_status,
-                 (rocrand_generator, Ptr{half}, Cint, half, half),
-                 generator, output_data, n, mean, stddev)
+    AMDGPU.prepare_state()
+    @check ccall((:rocrand_generate_normal_half, librocrand), rocrand_status, (rocrand_generator, Ptr{half}, Cint, half, half), generator, output_data, n, mean, stddev)
 end
 
 function rocrand_generate_log_normal(generator, output_data, n, mean, stddev)
-    @check ccall((:rocrand_generate_log_normal, librocrand), rocrand_status,
-                 (rocrand_generator, Ptr{Cfloat}, Cint, Cfloat, Cfloat),
-                 generator, output_data, n, mean, stddev)
+    AMDGPU.prepare_state()
+    @check ccall((:rocrand_generate_log_normal, librocrand), rocrand_status, (rocrand_generator, Ptr{Cfloat}, Cint, Cfloat, Cfloat), generator, output_data, n, mean, stddev)
 end
 
 function rocrand_generate_log_normal_double(generator, output_data, n, mean, stddev)
-    @check ccall((:rocrand_generate_log_normal_double, librocrand), rocrand_status,
-                 (rocrand_generator, Ptr{Cdouble}, Cint, Cdouble, Cdouble),
-                 generator, output_data, n, mean, stddev)
+    AMDGPU.prepare_state()
+    @check ccall((:rocrand_generate_log_normal_double, librocrand), rocrand_status, (rocrand_generator, Ptr{Cdouble}, Cint, Cdouble, Cdouble), generator, output_data, n, mean, stddev)
 end
 
 function rocrand_generate_log_normal_half(generator, output_data, n, mean, stddev)
-    @check ccall((:rocrand_generate_log_normal_half, librocrand), rocrand_status,
-                 (rocrand_generator, Ptr{half}, Cint, half, half),
-                 generator, output_data, n, mean, stddev)
+    AMDGPU.prepare_state()
+    @check ccall((:rocrand_generate_log_normal_half, librocrand), rocrand_status, (rocrand_generator, Ptr{half}, Cint, half, half), generator, output_data, n, mean, stddev)
 end
 
 function rocrand_generate_poisson(generator, output_data, n, lambda)
-    @check ccall((:rocrand_generate_poisson, librocrand), rocrand_status,
-                 (rocrand_generator, Ptr{UInt32}, Cint, Cdouble),
-                 generator, output_data, n, lambda)
+    AMDGPU.prepare_state()
+    @check ccall((:rocrand_generate_poisson, librocrand), rocrand_status, (rocrand_generator, Ptr{UInt32}, Cint, Cdouble), generator, output_data, n, lambda)
 end
 
 function rocrand_initialize_generator(generator)
-    @check ccall((:rocrand_initialize_generator, librocrand), rocrand_status,
-                 (rocrand_generator,),
-                 generator)
+    AMDGPU.prepare_state()
+    @check ccall((:rocrand_initialize_generator, librocrand), rocrand_status, (rocrand_generator,), generator)
 end
 
 function rocrand_set_stream(generator, stream)
-    @check ccall((:rocrand_set_stream, librocrand), rocrand_status,
-                 (rocrand_generator, hipStream_t),
-                 generator, stream)
+    AMDGPU.prepare_state()
+    @check ccall((:rocrand_set_stream, librocrand), rocrand_status, (rocrand_generator, hipStream_t), generator, stream)
 end
 
 function rocrand_set_seed(generator, seed)
-    @check ccall((:rocrand_set_seed, librocrand), rocrand_status,
-                 (rocrand_generator, Culonglong),
-                 generator, seed)
+    AMDGPU.prepare_state()
+    @check ccall((:rocrand_set_seed, librocrand), rocrand_status, (rocrand_generator, Culonglong), generator, seed)
 end
 
 function rocrand_set_offset(generator, offset)
-    @check ccall((:rocrand_set_offset, librocrand), rocrand_status,
-                 (rocrand_generator, Culonglong),
-                 generator, offset)
+    AMDGPU.prepare_state()
+    @check ccall((:rocrand_set_offset, librocrand), rocrand_status, (rocrand_generator, Culonglong), generator, offset)
 end
 
 function rocrand_set_quasi_random_generator_dimensions(generator, dimensions)
-    @check ccall((:rocrand_set_quasi_random_generator_dimensions, librocrand), rocrand_status,
-                 (rocrand_generator, UInt32),
-                 generator, dimensions)
+    AMDGPU.prepare_state()
+    @check ccall((:rocrand_set_quasi_random_generator_dimensions, librocrand), rocrand_status, (rocrand_generator, UInt32), generator, dimensions)
 end
 
 function rocrand_get_version(version)
-    @check ccall((:rocrand_get_version, librocrand), rocrand_status,
-                 (Ptr{Cint},),
-                 version)
+    AMDGPU.prepare_state()
+    @check ccall((:rocrand_get_version, librocrand), rocrand_status, (Ptr{Cint},), version)
 end
 
 function rocrand_create_poisson_distribution(lambda, discrete_distribution)
-    @check ccall((:rocrand_create_poisson_distribution, librocrand), rocrand_status,
-                 (Cdouble, Ptr{rocrand_discrete_distribution}),
-                 lambda, discrete_distribution)
+    AMDGPU.prepare_state()
+    @check ccall((:rocrand_create_poisson_distribution, librocrand), rocrand_status, (Cdouble, Ptr{rocrand_discrete_distribution}), lambda, discrete_distribution)
 end
 
-function rocrand_create_discrete_distribution(probabilities, size, offset,
-                                              discrete_distribution)
-    @check ccall((:rocrand_create_discrete_distribution, librocrand), rocrand_status,
-                 (Ptr{Cdouble}, UInt32, UInt32, Ptr{rocrand_discrete_distribution}),
-                 probabilities, size, offset, discrete_distribution)
+function rocrand_create_discrete_distribution(probabilities, size, offset, discrete_distribution) AMDGPU.prepare_state()
+    AMDGPU.prepare_state()
+    @check ccall((:rocrand_create_discrete_distribution, librocrand), rocrand_status, (Ptr{Cdouble}, UInt32, UInt32, Ptr{rocrand_discrete_distribution}), probabilities, size, offset, discrete_distribution)
 end
 
 function rocrand_destroy_discrete_distribution(discrete_distribution)
-    @check ccall((:rocrand_destroy_discrete_distribution, librocrand), rocrand_status,
-                 (rocrand_discrete_distribution,),
-                 discrete_distribution)
+    AMDGPU.prepare_state()
+    @check ccall((:rocrand_destroy_discrete_distribution, librocrand), rocrand_status, (rocrand_discrete_distribution,), discrete_distribution)
 end
