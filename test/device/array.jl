f203bccc8fb355a0db3f467dc92b43680f0d53e7
Anton Smirnov <tonysmn97@gmail.com>
2023-10-25 13:02:18 +0300

    Refactor

--- test/device/array.jl
+++ test/device/array.jl
@@ -2,19 +2,17 @@
     RA = ROCArray(rand(4,4))
     RD = rocconvert(RA)
     @test RD isa AMDGPU.Device.ROCDeviceMatrix
-    @show typeof(RD)
 
     # Host indexing is disallowed
     @test_throws Exception RD[1]
     @test_throws Exception RD[1,1]
+
     RD_view = @view RD[2:3, 2:3]
     @test RD_view isa SubArray
     @test_throws Exception RD_view[1]
     @test_throws Exception RD_view[1,1]
+
     RD_adj = RD'
-    @show typeof(RD_adj)
-    @show typeof(RD_adj) <: AMDGPU.Device.AnyROCDeviceVector
-    @show typeof(RD_adj) <: AMDGPU.Device.AnyROCDeviceArray
     @test RD_adj isa LinearAlgebra.Adjoint
     @test_throws Exception RD_adj[1]
     @test_throws Exception RD_adj[1,1]
