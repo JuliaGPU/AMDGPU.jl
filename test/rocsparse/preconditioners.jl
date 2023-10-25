16d425d7dc8f2a51314c860deb5ff05c886691a6
Alexis Montoison <alexis.montoison@polymtl.ca>
2023-10-25 13:40:09 -0500

    Add tests with triangular solves

--- test/rocsparse/preconditioners.jl
+++ test/rocsparse/preconditioners.jl
@@ -1,4 +1,5 @@
 n = 32
+p = 5
 blockdim = 4
 
 @testset "Preconditioners" begin
@@ -7,7 +8,7 @@ blockdim = 4
             A = rand(elty,n,n) + I
             A = sparse(A)
             d_A = SparseMatrixType == ROCSparseMatrixBSR ? SparseMatrixType(ROCSparseMatrixCSR(A), blockdim) : SparseMatrixType(A)
-            d_P = rocSPARSE.ilu0(d_A, '0')
+            d_P = rocSPARSE.ilu0(d_A, 'O')
             @test mapreduce(isfinite, &, collect(nonzeros(d_P)))
         end
 
@@ -15,8 +16,50 @@ blockdim = 4
             A = rand(elty,n,n)
             A = A * A'
             d_A = SparseMatrixType == ROCSparseMatrixBSR ? SparseMatrixType(ROCSparseMatrixCSR(A), blockdim) : SparseMatrixType(A)
-            d_P = rocSPARSE.ic0(d_A, '0')
+            d_P = rocSPARSE.ic0(d_A, 'O')
             @test mapreduce(isfinite, &, collect(nonzeros(d_P)))
+        end
+    end
+end
+
+@testset "Triangular solves" begin
+    @testset "$SparseMatrixType" for SparseMatrixType in (ROCSparseMatrixCSC, ROCSparseMatrixCSR, ROCSparseMatrixBSR)
+        @testset "y = T \ x -- $elty" for elty in (Float32, Float64, ComplexF32, ComplexF64)
+            @testset "trans = $trans" for (trans, op) in (('N', identity), ('T', transpose), ('C', adjoint))
+                (SparseMatrixType == ROCSparseMatrixCSC) && (trans == 'C') && (elty <: Complex) && continue
+                @testset "trans = $uplo" for uplo in ('L', 'U')
+                    @testset "diag = $diag" for diag in ('N', 'U')
+                        T = rand(elty,n,n)
+                        T = uplo == 'L' ? tril(T) : triu(T)
+                        T = diag == 'N' ? T : T - Diagonal(T) + I
+                        d_T = SparseMatrixType == ROCSparseMatrixBSR ? SparseMatrixType(ROCSparseMatrixCSR(T), blockdim) : SparseMatrixType(T)
+                        x = rand(elty,n)
+                        d_x = ROCVector{elty}(x)
+                        d_y = rocSPARSE.sv2(trans, uplo, diag, d_T, d_x, 'O')
+                        y = op(T) \ x
+                        @test collect(d_y) ≈ y
+                    end
+                end
+            end
+        end
+
+        @testset "Y = T \ X -- $elty" for elty in (Float32, Float64, ComplexF32, ComplexF64)
+            @testset "trans = $trans" for (trans, op) in (('N', identity), ('T', transpose), ('C', adjoint))
+                (SparseMatrixType == ROCSparseMatrixCSC) && (trans == 'C') && (elty <: Complex) && continue
+                @testset "trans = $uplo" for uplo in ('L', 'U')
+                    @testset "diag = $diag" for diag in ('N', 'U')
+                        T = rand(elty,n,n)
+                        T = uplo == 'L' ? tril(T) : triu(T)
+                        T = diag == 'N' ? T : T - Diagonal(T) + I
+                        d_T = SparseMatrixType == ROCSparseMatrixBSR ? SparseMatrixType(ROCSparseMatrixCSR(T), blockdim) : SparseMatrixType(T)
+                        X = rand(elty,n,p)
+                        d_X = ROCMatrix{elty}(X)
+                        d_Y = rocSPARSE.sm2(trans, uplo, diag, d_T, d_X, 'O')
+                        Y = op(T) \ X
+                        @test collect(d_Y) ≈ Y
+                    end
+                end
+            end
         end
     end
 end
