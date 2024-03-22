625b00a85c0d8bf96710ad00a00536852e815d5d
Alexis Montoison <alexis.montoison@polymtl.ca>
2024-03-21 21:03:01 -0400

    [rocSOLVER] Add a test for ormqr!

--- test/rocarray/solver.jl
+++ test/rocarray/solver.jl
@@ -137,13 +137,37 @@ end
     end
 end
 
-@testset "geqrf! -- omgqr!" begin
+@testset "geqrf! -- orgqr!" begin
     @testset "elty = $elty" for elty in [Float32, Float64, ComplexF32, ComplexF64]
         A = rand(elty, m, n)
         dA = ROCArray(A)
         dA, τ = rocSOLVER.geqrf!(dA)
         rocSOLVER.orgqr!(dA, τ)
         @test dA' * dA ≈ I
+    end
+end
+
+@testset "ormqr!" begin
+    @testset "elty = $elty" for elty in [Float32, Float64, ComplexF32, ComplexF64]
+        @testset "side = $side" for side in ['L', 'R']
+            @testset "trans = $trans" for (trans, op) in [('N', identity), ('T', transpose), ('C', adjoint)]
+                A = rand(elty, m, n)
+                dA = ROCArray(A)
+                dA, dτ = rocSOLVER.geqrf!(dA)
+
+                hI = Matrix{elty}(I, m, m)
+                dI = ROCArray(hI)
+                dH = rocSOLVER.ormqr!(side, 'N', dA, dτ, dI)
+                @test dH' * dH ≈ I
+
+                C = side == 'L' ? rand(elty, m, n) : rand(elty, n, m)
+                dC = ROCArray(C)
+                dD = side == 'L' ? op(dH) * dC : dC * op(dH)
+
+                ormqr!(side, trans, dA, dτ, dC)
+                @test dC ≈ dD
+            end
+        end
     end
 end
 
