d264d11152d2a6e5492f423bd2bcdb2ae7844075
Anton Smirnov <tonysmn97@gmail.com>
2024-07-06 13:09:42 +0300

    Run TLS tests on Navi 3 again

--- test/core_tests.jl
+++ test/core_tests.jl
@@ -85,9 +85,6 @@ include("codegen/trap.jl")
 include("rocarray/base.jl")
 include("rocarray/broadcast.jl")
 
-const IS_NAVI3 = AMDGPU.device().gcn_arch in ("gfx1100", "gfx1101", "gfx1102", "gfx1103")
-if !IS_NAVI3
-    include("tls.jl") # TODO hangs on Navi 3
-end
+include("tls.jl")
 
 end
