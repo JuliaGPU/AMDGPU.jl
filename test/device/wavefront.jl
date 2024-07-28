fc9b8e12744d5368cdf7ce09ab3d1b5303a69b8e
Anton Smirnov <tonysmn97@gmail.com>
2024-07-28 16:08:28 +0300

    Fix

--- test/device/wavefront.jl
+++ test/device/wavefront.jl
@@ -1,63 +1,63 @@
-@testset "Wavefront Shuffle" begin
-    ws = AMDGPU.HIP.wavefrontsize(AMDGPU.device())
+# @testset "Wavefront Shuffle" begin
+#     ws = AMDGPU.HIP.wavefrontsize(AMDGPU.device())
 
-    # # Shift values between lanes by 1 to the left in a wavefront.
-    # function ker_shfl!(x, ::Type{T}, ws) where T
-    #     i = AMDGPU.Device.activelane()
-    #     x[i + 1] = AMDGPU.Device.shfl(T(i), i + 1, ws)
-    #     return
-    # end
+#     # Shift values between lanes by 1 to the left in a wavefront.
+#     function ker_shfl!(x, ::Type{T}, ws) where T
+#         i = AMDGPU.Device.activelane()
+#         x[i + 1] = AMDGPU.Device.shfl(T(i), i + 1, ws)
+#         return
+#     end
 
-    # # Same as above, but fill both high and low bits.
-    # function ker_shfl_composed!(x, ::Type{T}, ws) where T
-    #     i = AMDGPU.Device.activelane()
-    #     v = T(i) << (sizeof(T) * 4) | T(i)
-    #     x[i + 1] = AMDGPU.Device.shfl(v, i + 1, ws)
-    #     return
-    # end
+#     # Same as above, but fill both high and low bits.
+#     function ker_shfl_composed!(x, ::Type{T}, ws) where T
+#         i = AMDGPU.Device.activelane()
+#         v = T(i) << (sizeof(T) * 4) | T(i)
+#         x[i + 1] = AMDGPU.Device.shfl(v, i + 1, ws)
+#         return
+#     end
 
-    # # Same as the first kernel, but instead test for even lane id.
-    # function ker_shfl_bool!(x, ws)
-    #     i = AMDGPU.Device.activelane()
-    #     x[i + 1] = AMDGPU.Device.shfl(i % 2 == 0, i + 1, ws)
-    #     return
-    # end
+#     # Same as the first kernel, but instead test for even lane id.
+#     function ker_shfl_bool!(x, ws)
+#         i = AMDGPU.Device.activelane()
+#         x[i + 1] = AMDGPU.Device.shfl(i % 2 == 0, i + 1, ws)
+#         return
+#     end
 
-    # @testset "shfl T=$T" for T in (
-    #     UInt8, UInt16, UInt32,
-    #     Int8, Int16, Int32,
-    #     Float16, Float32, Float64,
-    # )
-    #     target = Array{T}(undef, ws)
-    #     for i in 1:ws
-    #         target[i] = i % ws
-    #     end
+#     @testset "shfl T=$T" for T in (
+#         UInt8, UInt16, UInt32,
+#         Int8, Int16, Int32,
+#         Float16, Float32, Float64,
+#     )
+#         target = Array{T}(undef, ws)
+#         for i in 1:ws
+#             target[i] = i % ws
+#         end
 
-    #     x = ROCArray{T}(undef, ws)
-    #     @roc groupsize=ws ker_shfl!(x, T, ws)
-    #     @test Array(x) == target
-    # end
+#         x = ROCArray{T}(undef, ws)
+#         @roc groupsize=ws ker_shfl!(x, T, ws)
+#         @test Array(x) == target
+#     end
 
-    # @testset "shfl composed T=$T" for T in (UInt64, UInt128, Int64, Int128)
-    #     target = Array{T}(undef, ws)
-    #     for i in 1:ws
-    #         v = T(i % ws)
-    #         v = (v << (sizeof(T) * 4)) | v
-    #         target[i] = v
-    #     end
+#     @testset "shfl composed T=$T" for T in (UInt64, UInt128, Int64, Int128)
+#         target = Array{T}(undef, ws)
+#         for i in 1:ws
+#             v = T(i % ws)
+#             v = (v << (sizeof(T) * 4)) | v
+#             target[i] = v
+#         end
 
-    #     x = ROCArray{T}(undef, ws)
-    #     @roc groupsize=ws ker_shfl_composed!(x, T, ws)
-    #     @test Array(x) == target
-    # end
+#         x = ROCArray{T}(undef, ws)
+#         @roc groupsize=ws ker_shfl_composed!(x, T, ws)
+#         @test Array(x) == target
+#     end
 
-    # @testset "shfl boolean" begin
-    #     target = [i % 2 == 0 for i in 1:ws]
+#     @testset "shfl boolean" begin
+#         target = [i % 2 == 0 for i in 1:ws]
 
-    #     x = ROCArray{Bool}(undef, ws)
-    #     @roc groupsize=ws ker_shfl_bool!(x, ws)
-    #     @test Array(x) == target
-    # end
+#         x = ROCArray{Bool}(undef, ws)
+#         @roc groupsize=ws ker_shfl_bool!(x, ws)
+#         @test Array(x) == target
+#     end
 # end
 
 @testset "Wavefront Operations" begin
