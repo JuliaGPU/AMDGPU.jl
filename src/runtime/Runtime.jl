38e9528ece4309e752cc67291b3a85559e3c3c41
Anton Smirnov <tonysmn97@gmail.com>
2024-09-24 11:41:46 +0300

    Cleanup

--- src/runtime/Runtime.jl
+++ src/runtime/Runtime.jl
@@ -6,7 +6,6 @@ using ..GPUCompiler
 import ..Adapt
 import Preferences: @load_preference, @set_preferences!
 
-import ..HSA
 import ..HIP
 import ..AMDGPU
 import ..AMDGPU: getinfo, LockedObject
@@ -17,14 +16,13 @@ struct Adaptor end
 const RT_LOCK = Threads.ReentrantLock()
 const RT_EXITING = Ref{Bool}(false)
 
-include("error.jl")
 include("dims.jl")
 
 module Mem
     using Preferences
 
     import AMDGPU
-    import AMDGPU: HIP, HSA, Runtime
+    import AMDGPU: HIP, Runtime
     import .HIP: HIPDevice, HIPContext
     import .Runtime: ROCDim, ROCDim3
 
