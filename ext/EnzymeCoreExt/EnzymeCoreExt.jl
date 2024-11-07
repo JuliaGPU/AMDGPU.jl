ebc01965459e3896f86cb561f0ccfa072969daac
Anton Smirnov <tonysmn97@gmail.com>
2024-11-07 23:45:52 +0200

    Update to newer Enzyme

--- ext/EnzymeCoreExt/EnzymeCoreExt.jl
+++ ext/EnzymeCoreExt/EnzymeCoreExt.jl
@@ -5,6 +5,8 @@ using EnzymeCore
 using EnzymeCore: EnzymeRules
 using GPUCompiler
 
+include("meta_kernels.jl")
+
 function EnzymeCore.compiler_job_from_backend(
     ::ROCBackend, @nospecialize(F::Type), @nospecialize(TT::Type),
 )
@@ -13,48 +15,87 @@ function EnzymeCore.compiler_job_from_backend(
 end
 
 function EnzymeRules.forward(
-    fn::Const{typeof(AMDGPU.hipfunction)}, ::Type{<: Duplicated},
-    f::Const{F}, tt::Const{TT}; kwargs...
+    config, fn::Const{typeof(AMDGPU.hipfunction)}, ::Type{<: Duplicated},
+    f::Const{F}, tt::Const{TT}; kwargs...,
 ) where {F, TT}
     res = fn.val(f.val, tt.val; kwargs...)
     return Duplicated(res, res)
 end
 
 function EnzymeRules.forward(
-    fn::Const{typeof(AMDGPU.hipfunction)}, ::Type{<: BatchDuplicated{T, N}},
-    f::Const{F}, tt::Const{TT}; kwargs...
+    config, fn::Const{typeof(AMDGPU.hipfunction)}, ::Type{<: BatchDuplicated{T, N}},
+    f::Const{F}, tt::Const{TT}; kwargs...,
 ) where {F, TT, T, N}
     res = fn.val(f.val, tt.val; kwargs...)
     return BatchDuplicated(res, ntuple(_ -> res, Val(N)))
 end
 
 function EnzymeRules.reverse(
-    config, fn::Const{typeof(AMDGPU.hipfunction)},
-    ::Type{RT}, subtape, f, tt; kwargs...,
+    config, fn::Const{typeof(AMDGPU.hipfunction)}, ::Type{RT},
+    subtape, f, tt; kwargs...,
 ) where RT
     return (nothing, nothing)
 end
 
 function EnzymeRules.forward(
-    fn::Const{typeof(AMDGPU.rocconvert)}, ::Type{RT}, x::IT,
+    config, fn::Const{typeof(AMDGPU.rocconvert)}, ::Type{RT}, x::IT,
 ) where {RT, IT}
-    if RT <: Duplicated
-        Duplicated(fn.val(x.val), fn.val(x.dval))
-    elseif RT <: Const
-        fn.val(x.val)::eltype(RT)
-    elseif RT <: DuplicatedNoNeed
-        fn.val(x.val)::eltype(RT)
-    else
-        tup = ntuple(Val(EnzymeCore.batch_size(RT))) do i
-            Base.@_inline_meta
-            fn.val(x.dval[i])::eltype(RT)
-        end
-        if RT <: BatchDuplicated
-            BatchDuplicated(ofv.val(x.val), tup)
+    if EnzymeRules.needs_primal(config) && EnzymeRules.needs_shadow(config)
+        config_width = EnzymeRules.width(config)
+        if config_width == 1
+            Duplicated(fn.val(x.val), fn.val(x.dval))
         else
-            tup
+            tup = ntuple(Val(config_width)) do i
+                Base.@_inline_meta
+                fn.val(x.dval[i])::eltype(RT)
+            end
+            BatchDuplicated(fn.val(x.val), tup)
         end
+
+    elseif EnzymeRules.needs_shadow(config)
+        config_width = EnzymeRules.width(config)
+        ST = EnzymeCore.shadow_type(config, RT)
+        if config_width == 1
+            fn.val(x.dval)::ST
+        else
+            (ntuple(Val(config_width)) do i
+                Base.@_inline_meta
+                fn.val(x.dval[i])::eltype(RT)
+            end)::ST
+        end
+
+    elseif EnzymeRules.needs_primal(config)
+        # TODO `uval` undefined?
+        fn.val(uval.val)::eltype(RT)
+    else
+        nothing
     end
+end
+
+function EnzymeRules.augmented_primal(
+    config, fn::Const{typeof(AMDGPU.rocconvert)}, ::Type{RT}, x::IT,
+) where {RT, IT}
+    primal = EnzymeRules.needs_primal(config) ?
+        fn.val(x.val) : nothing
+
+    shadow = if EnzymeRules.needs_shadow(config)
+        config_width = EnzymeRules.width(config)
+        if config_width == 1
+            fn.val(x.dval)
+        else
+            ntuple(Val(config_width)) do i
+                Base.@_inline_meta
+                fn.val(x.dval[i])
+            end
+        end
+    else
+        nothing
+    end
+
+    return EnzymeRules.AugmentedReturn{
+        EnzymeRules.primal_type(config, RT),
+        EnzymeRules.shadow_type(config, RT), Nothing
+    }(primal, shadow, nothing)
 end
 
 function EnzymeRules.reverse(
@@ -64,116 +105,81 @@ function EnzymeRules.reverse(
     return (nothing,)
 end
 
-function meta_fn(fn, args::Vararg{Any, N}) where N
-    EnzymeCore.autodiff_deferred(Forward, fn, Const, args...)
-    return
-end
-
 function EnzymeRules.forward(
-    fn::EnzymeCore.Annotation{AMDGPU.Runtime.HIPKernel{F, TT}},
+    config, fn::EnzymeCore.Annotation{AMDGPU.Runtime.HIPKernel{F, TT}},
     ::Type{Const{Nothing}}, args...; kwargs...,
 ) where {F, TT}
     GC.@preserve args begin
         kernel_args = ((rocconvert(a) for a in args)...,)
-        kernel_tt = Tuple{(F, (typeof(a) for a in kernel_args)...)...}
+        kernel_tt = Tuple{(typeof(config), F, (typeof(a) for a in kernel_args)...)...}
         kernel = AMDGPU.hipfunction(meta_fn, kernel_tt)
-        kernel(fn.val.f, args...; kwargs...)
+        kernel(config, fn.val.f, kernel_args...; kwargs...)
     end
     return
 end
 
+function EnzymeRules.reverse(
+    config, ofn::EnzymeCore.Annotation{AMDGPU.Runtime.HIPKernel{F, TT}},
+    ::Type{Const{Nothing}}, subtape, args...;
+    groupsize::AMDGPU.Runtime.ROCDim = 1,
+    gridsize::AMDGPU.Runtime.ROCDim = 1,
+    kwargs...,
+) where {F, TT}
+    kernel_args = ((rocconvert(a) for a in args)...,)
+    kernel_tt = map(typeof, kernel_args)
+    ModifiedBetween = EnzymeRules.overwritten(config)
+    TapeType = EnzymeCore.tape_type(
+        ReverseSplitModified(
+            EnzymeCore.set_runtime_activity(ReverseSplitWithPrimal, config),
+            Val(ModifiedBetween)),
+        Const{F},
+        Const{Nothing},
+        kernel_tt...,
+    )
+    groupsize = AMDGPU.Runtime.ROCDim3(groupsize)
+    gridsize = AMDGPU.Runtime.ROCDim3(gridsize)
+
+    GC.@preserve args subtape begin
+        subtape_cc = rocconvert(subtape)
+        kernel_tt2 = Tuple{
+            (typeof(config), F, typeof(subtape_cc), kernel_tt...)...}
+        kernel = AMDGPU.hipfunction(meta_revf, kernel_tt2)
+        kernel(config, ofn.val.f, subtape_cc, args...;
+            groupsize, gridsize, kwargs...)
+    end
+
+    return ntuple(Val(length(kernel_args))) do i
+        Base.@_inline_meta
+        nothing
+    end
+end
+
 function EnzymeRules.augmented_primal(
-    config, fn::Const{typeof(AMDGPU.rocconvert)}, ::Type{RT}, x::IT,
-) where {RT, IT}
-    primal = EnzymeRules.needs_primal(config) ?
-        fn.val(x.val) : nothing
-    primal_T = EnzymeRules.needs_primal(config) ? eltype(RT) : Nothing
+    config, fn::Const{typeof(AMDGPU.hipfunction)},
+    ::Type{RT}, f::Const{F}, tt::Const{TT}; kwargs...
+) where {F, CT, RT <: EnzymeCore.Annotation{CT}, TT}
+    res = fn.val(f.val, tt.val; kwargs...)
+
+    primal = EnzymeRules.needs_primal(config) ? res : nothing
 
     shadow = if EnzymeRules.needs_shadow(config)
-        if EnzymeRules.width(config) == 1
-            fn.val(x.dval)
+        config_width = EnzymeRules.width(config)
+        if config_width == 1
+            res
         else
-            ntuple(Val(EnzymeRules.width(config))) do i
+            ntuple(Val(config_width)) do i
                 Base.@_inline_meta
-                fn.val(x.dval[i])
+                res
             end
         end
     else
         nothing
     end
-    shadow_T = EnzymeRules.needs_shadow(config) ?
-        (EnzymeRules.width(config) == 1 ?
-            eltype(RT) : NTuple{EnzymeRules.width(config), eltype(RT)}) :
-        Nothing
 
-    return EnzymeRules.AugmentedReturn{primal_T, shadow_T, Nothing}(
-        primal, shadow, nothing)
-end
-
-function EnzymeRules.augmented_primal(
-    config, fn::Const{typeof(AMDGPU.hipfunction)},
-    ::Type{RT}, f::Const{F},
-    tt::Const{TT}; kwargs...
-) where {F, CT, RT <: EnzymeCore.Annotation{CT}, TT}
-    res = fn.val(f.val, tt.val; kwargs...)
-
-    primal = EnzymeRules.needs_primal(config) ? res : nothing
-    primal_T = EnzymeRules.needs_primal(config) ? CT : Nothing
-
-    shadow = if EnzymeRules.needs_shadow(config)
-        if EnzymeRules.width(config) == 1
-            res
-        else
-          ntuple(Val(EnzymeRules.width(config))) do i
-              Base.@_inline_meta
-              res
-          end
-        end
-    else
-        nothing
-    end
-    shadow_T = EnzymeRules.needs_shadow(config) ?
-        (EnzymeRules.width(config) == 1 ?
-            CT : NTuple{EnzymeRules.width(config), CT}) :
-        Nothing
-
-    return EnzymeRules.AugmentedReturn{primal_T, shadow_T, Nothing}(
-        primal, shadow, nothing)
-end
-
-function meta_augf(
-    f, tape::ROCDeviceArray{TapeType}, ::Val{ModifiedBetween}, args::Vararg{Any, N},
-) where {N, ModifiedBetween, TapeType}
-    forward, _ = EnzymeCore.autodiff_deferred_thunk(
-        ReverseSplitModified(ReverseSplitWithPrimal, Val(ModifiedBetween)),
-        TapeType,
-        Const{Core.Typeof(f)},
-        Const{Nothing},
-        map(typeof, args)...,
-    )
-
-    idx = 0
-    # idx *= gridDim().x
-    idx += workgroupIdx().x - 1
-
-    idx *= gridGroupDim().y
-    idx += workgroupIdx().y - 1
-
-    idx *= gridGroupDim().z
-    idx += workgroupIdx().z - 1
-
-    idx *= workgroupDim().x
-    idx += workitemIdx().x - 1
-
-    idx *= workgroupDim().y
-    idx += workitemIdx().y - 1
-
-    idx *= workgroupDim().z
-    idx += workitemIdx().z - 1
-    idx += 1
-
-    @inbounds tape[idx] = forward(Const(f), args...)[1]
-    return
+    return EnzymeRules.AugmentedReturn{
+        EnzymeRules.primal_type(config, RT),
+        EnzymeRules.shadow_type(config, RT), Nothing,
+    }(primal, shadow, nothing)
 end
 
 function EnzymeRules.augmented_primal(
@@ -190,25 +196,25 @@ function EnzymeRules.augmented_primal(
         ROCBackend(), typeof(Base.identity), Tuple{Float64})
     TapeType = EnzymeCore.tape_type(
         compiler_job,
-        ReverseSplitModified(ReverseSplitWithPrimal, Val(ModifiedBetween)),
+        ReverseSplitModified(
+            EnzymeCore.set_runtime_activity(ReverseSplitWithPrimal, config),
+            Val(ModifiedBetween)),
         Const{F}, Const{Nothing},
         kernel_tt...,
     )
-    threads = AMDGPU.Runtime.ROCDim3(groupsize)
-    blocks = AMDGPU.Runtime.ROCDim3(gridsize)
-    subtape = ROCArray{TapeType}(
-        undef, blocks.x * blocks.y * blocks.z * threads.x * threads.y * threads.z)
+    groupsize = AMDGPU.Runtime.ROCDim3(groupsize)
+    gridsize = AMDGPU.Runtime.ROCDim3(gridsize)
+    subtape = ROCArray{TapeType}(undef,
+        gridsize.x * gridsize.y * gridsize.z *
+        groupsize.x * groupsize.y * groupsize.z)
 
     GC.@preserve args subtape begin
         subtape_cc = rocconvert(subtape)
-        kernel_tt2 = Tuple{(
-            F, typeof(subtape_cc), Val{ModifiedBetween}, kernel_tt...,
-        )...}
+        kernel_tt2 = Tuple{
+            (typeof(config), F, typeof(subtape_cc), Val{ModifiedBetween}, kernel_tt...)...}
         kernel = AMDGPU.hipfunction(meta_augf, kernel_tt2)
-        kernel(fn.val.f, subtape_cc, Val(ModifiedBetween), args...;
-            groupsize=(groupsize.x, groupsize.y, groupsize.z),
-            gridsize=(gridsize.x, gridsize.y, gridsize.z),
-            kwargs...)
+        kernel(config, fn.val.f, subtape_cc, Val(ModifiedBetween), args...;
+            groupsize, gridsize, kwargs...)
     end
     return AugmentedReturn{Nothing, Nothing, ROCArray}(nothing, nothing, subtape)
 end
