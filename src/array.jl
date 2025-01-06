ea7823689946823c9c268eefdd1f942edeb8bfb4
Anton Smirnov <tonysmn97@gmail.com>
2025-01-07 00:21:29 +0200

    Upd

--- src/array.jl
+++ src/array.jl
@@ -13,16 +13,14 @@ mutable struct ROCArray{T, N, B} <: AbstractGPUArray{T, N}
             data = DataRef(pool_free, pool_alloc(B, sz))
             finalizer(unsafe_free!, new{T, N, B}(data, dims, 0))
         end
-        return _alloc_f()
+        # return _alloc_f()
 
-        # name = GPUArrays.CacheAllocatorName[]
-        # # Do not use caching allocator if it is not set or
-        # # the buffer is not a device memory.
-        # return if !(B <: Mem.HIPBuffer) || name == :none
-        #     _alloc_f()
-        # else
-        #     GPUArrays.alloc!(_alloc_f, ROCBackend(), name, T, dims)::ROCArray{T, N, B}
-        # end
+        name = GPUArrays.AllocCache.CacheAllocatorName[]
+        return if name ≡ nothing
+            _alloc_f()
+        else
+            GPUArrays.AllocCache.alloc!(_alloc_f, ROCArray, name, (B, T, dims))::ROCArray{T, N, B}
+        end
     end
 
     function ROCArray{T, N}(
