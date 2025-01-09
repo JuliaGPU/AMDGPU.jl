bf60d4e37e6d94356d70144187c21f41bfc3e391
Anton Smirnov <tonysmn97@gmail.com>
2025-01-09 17:08:19 +0200

    Uncomment tests

--- test/enzyme_tests.jl
+++ test/enzyme_tests.jl
@@ -30,26 +30,26 @@ end
     Enzyme.autodiff(Forward, square!, Duplicated(A, dA))
     @test all(dA .≈ (2:2:128))
 
-    # A = ROCArray(collect(1.0:64.0))
-    # dA = ROCArray(ones(Float64, 64))
-    # dA2 = ROCArray(ones(Float64, 64) .* 3.0)
-    # Enzyme.autodiff(Forward, square!, BatchDuplicated(A, (dA, dA2)))
-    # @test all(dA .≈ (2:2:128))
-    # @test all(dA2 .≈ (2:2:128) .* 3)
+    A = ROCArray(collect(1.0:64.0))
+    dA = ROCArray(ones(Float64, 64))
+    dA2 = ROCArray(ones(Float64, 64) .* 3.0)
+    Enzyme.autodiff(Forward, square!, BatchDuplicated(A, (dA, dA2)))
+    @test all(dA .≈ (2:2:128))
+    @test all(dA2 .≈ (2:2:128) .* 3)
 end
 
-# @testset "Reverse Kernel" begin
-#     A = ROCArray(collect(1.0:64.0))
-#     dA = ROCArray(ones(Float64, 64))
-#     Enzyme.autodiff(Reverse, square!, Duplicated(A, dA))
-#     @test all(dA .≈ (2:2:128))
+@testset "Reverse Kernel" begin
+    A = ROCArray(collect(1.0:64.0))
+    dA = ROCArray(ones(Float64, 64))
+    Enzyme.autodiff(Reverse, square!, Duplicated(A, dA))
+    @test all(dA .≈ (2:2:128))
 
-#     # A = ROCArray(collect(1.0:64.0))
-#     # dA = ROCArray(ones(Float64, 64))
-#     # dA2 = ROCArray(ones(Float64, 64) .* 3.0)
-#     # Enzyme.autodiff(Reverse, square!, BatchDuplicated(A, (dA, dA2)))
-#     # @test all(dA .≈ (2:2:128))
-#     # @test all(dA2 .≈ (2:2:128) .* 3)
-# end
+    A = ROCArray(collect(1.0:64.0))
+    dA = ROCArray(ones(Float64, 64))
+    dA2 = ROCArray(ones(Float64, 64) .* 3.0)
+    Enzyme.autodiff(Reverse, square!, BatchDuplicated(A, (dA, dA2)))
+    @test all(dA .≈ (2:2:128))
+    @test all(dA2 .≈ (2:2:128) .* 3)
+end
 
 end
