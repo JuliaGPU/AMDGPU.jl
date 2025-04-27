e31cc69949a6e98b683962064c4d1987eb397c1b
Anton Smirnov <tonysmn97@gmail.com>
2025-04-28 01:22:36 +0300

    Disable some tests

--- test/device_tests.jl
+++ test/device_tests.jl
@@ -10,12 +10,17 @@ include("device/vadd.jl")
 include("device/memory.jl")
 include("device/indexing.jl")
 include("device/wavefront.jl")
-include("device/synchronization.jl")
 include("device/execution_control.jl")
 include("device/exceptions.jl")
 include("device/random.jl")
 
 # TODO https://github.com/JuliaGPU/AMDGPU.jl/issues/546
 include("device/math.jl")
+
+# TODO broken because of device libraries using intrinsics from LLVM 19.
+# Fix: recompile device libraries with LLVM 18 and ship as artifacts.
+if VERSION < v"1.12-"
+    include("device/synchronization.jl")
+end
 
 end
