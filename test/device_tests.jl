e593161c0e0eaa340c1ef11496c1ea745bb2c42e
Anton Smirnov <tonysmn97@gmail.com>
2025-04-28 22:54:12 +0300

    Use artifact device libraries

--- test/device_tests.jl
+++ test/device_tests.jl
@@ -4,23 +4,16 @@ using AMDGPU
 using AMDGPU: Device, Runtime, @allowscalar
 import LinearAlgebra
 
-include("device/launch.jl")
 include("device/array.jl")
-include("device/vadd.jl")
-include("device/memory.jl")
-include("device/indexing.jl")
-include("device/wavefront.jl")
 include("device/execution_control.jl")
 include("device/exceptions.jl")
-include("device/random.jl")
-
-# TODO https://github.com/JuliaGPU/AMDGPU.jl/issues/546
+include("device/indexing.jl")
+include("device/launch.jl")
 include("device/math.jl")
-
-# TODO broken because of device libraries using intrinsics from LLVM 19.
-# Fix: recompile device libraries with LLVM 18 and ship as artifacts.
-if VERSION < v"1.12-"
-    include("device/synchronization.jl")
-end
+include("device/memory.jl")
+include("device/wavefront.jl")
+include("device/random.jl")
+include("device/synchronization.jl")
+include("device/vadd.jl")
 
 end
