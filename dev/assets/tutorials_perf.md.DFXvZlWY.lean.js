2dddc27a7f41efc8e1530092093c60549df3c736
Documenter.jl <documenter@juliadocs.github.io>
2025-05-08 20:22:22 +0000

    build based on 3d07a5db

--- dev/assets/tutorials_perf.md.DFXvZlWY.lean.js
+++ dev/assets/tutorials_perf.md.DFXvZlWY.lean.js
@@ -1,13 +1,13 @@
 import{_ as h,C as e,c as k,o as r,aj as d,G as n,w as l,j as s,a as i}from"./chunks/framework.wl6OA9_5.js";const f=JSON.parse('{"title":"Performance Tips","description":"","frontmatter":{},"headers":[],"relativePath":"tutorials/perf.md","filePath":"tutorials/perf.md","lastUpdated":null}'),o={name:"tutorials/perf.md"};function c(E,a,g,y,u,F){const t=e("PluginTabsTab"),p=e("PluginTabs");return r(),k("div",null,[a[2]||(a[2]=d("",21)),n(p,null,{default:l(()=>[n(t,{label:"unsafe_trunc(Int, 1.0)"},{default:l(()=>a[0]||(a[0]=[s("div",{class:"language-julia vp-adaptive-theme"},[s("button",{title:"Copy Code",class:"copy"}),s("span",{class:"lang"},"julia"),s("pre",{class:"shiki shiki-themes github-light github-dark vp-code",tabindex:"0"},[s("code",null,[s("span",{class:"line"},[s("span",{style:{"--shiki-light":"#D73A49","--shiki-dark":"#F97583"}},"using"),s("span",{style:{"--shiki-light":"#24292E","--shiki-dark":"#E1E4E8"}}," InteractiveUtils")]),i(`
 `),s("span",{class:"line"},[s("span",{style:{"--shiki-light":"#24292E","--shiki-dark":"#E1E4E8"}},"InteractiveUtils"),s("span",{style:{"--shiki-light":"#D73A49","--shiki-dark":"#F97583"}},"."),s("span",{style:{"--shiki-light":"#005CC5","--shiki-dark":"#79B8FF"}},"@code_llvm"),s("span",{style:{"--shiki-light":"#005CC5","--shiki-dark":"#79B8FF"}}," unsafe_trunc"),s("span",{style:{"--shiki-light":"#24292E","--shiki-dark":"#E1E4E8"}},"(Int, "),s("span",{style:{"--shiki-light":"#005CC5","--shiki-dark":"#79B8FF"}},"1.0"),s("span",{style:{"--shiki-light":"#24292E","--shiki-dark":"#E1E4E8"}},")")])])])],-1),s("div",{class:"language- vp-adaptive-theme"},[s("button",{title:"Copy Code",class:"copy"}),s("span",{class:"lang"}),s("pre",{class:"shiki shiki-themes github-light github-dark vp-code",tabindex:"0"},[s("code",null,[s("span",{class:"line"},[s("span",null,";  @ float.jl:336 within `unsafe_trunc`")]),i(`
-`),s("span",{class:"line"},[s("span",null,"define i64 @julia_unsafe_trunc_6472(double %0) #0 {")]),i(`
+`),s("span",{class:"line"},[s("span",null,"define i64 @julia_unsafe_trunc_6452(double %0) #0 {")]),i(`
 `),s("span",{class:"line"},[s("span",null,"top:")]),i(`
 `),s("span",{class:"line"},[s("span",null,"  %1 = fptosi double %0 to i64")]),i(`
 `),s("span",{class:"line"},[s("span",null,"  %2 = freeze i64 %1")]),i(`
 `),s("span",{class:"line"},[s("span",null,"  ret i64 %2")]),i(`
 `),s("span",{class:"line"},[s("span",null,"}")])])])],-1)])),_:1}),n(t,{label:"Int(1.0)"},{default:l(()=>a[1]||(a[1]=[s("div",{class:"language-julia vp-adaptive-theme"},[s("button",{title:"Copy Code",class:"copy"}),s("span",{class:"lang"},"julia"),s("pre",{class:"shiki shiki-themes github-light github-dark vp-code",tabindex:"0"},[s("code",null,[s("span",{class:"line"},[s("span",{style:{"--shiki-light":"#D73A49","--shiki-dark":"#F97583"}},"using"),s("span",{style:{"--shiki-light":"#24292E","--shiki-dark":"#E1E4E8"}}," InteractiveUtils")]),i(`
 `),s("span",{class:"line"},[s("span",{style:{"--shiki-light":"#24292E","--shiki-dark":"#E1E4E8"}},"InteractiveUtils"),s("span",{style:{"--shiki-light":"#D73A49","--shiki-dark":"#F97583"}},"."),s("span",{style:{"--shiki-light":"#005CC5","--shiki-dark":"#79B8FF"}},"@code_llvm"),s("span",{style:{"--shiki-light":"#005CC5","--shiki-dark":"#79B8FF"}}," Int"),s("span",{style:{"--shiki-light":"#24292E","--shiki-dark":"#E1E4E8"}},"("),s("span",{style:{"--shiki-light":"#005CC5","--shiki-dark":"#79B8FF"}},"1.0"),s("span",{style:{"--shiki-light":"#24292E","--shiki-dark":"#E1E4E8"}},")")])])])],-1),s("div",{class:"language- vp-adaptive-theme"},[s("button",{title:"Copy Code",class:"copy"}),s("span",{class:"lang"}),s("pre",{class:"shiki shiki-themes github-light github-dark vp-code",tabindex:"0"},[s("code",null,[s("span",{class:"line"},[s("span",null,";  @ float.jl:908 within `Int64`")]),i(`
-`),s("span",{class:"line"},[s("span",null,"define i64 @julia_Int64_6474(double %0) #0 {")]),i(`
+`),s("span",{class:"line"},[s("span",null,"define i64 @julia_Int64_6454(double %0) #0 {")]),i(`
 `),s("span",{class:"line"},[s("span",null,"top:")]),i(`
 `),s("span",{class:"line"},[s("span",null,"  %gcframe10 = alloca [6 x {}*], align 16")]),i(`
 `),s("span",{class:"line"},[s("span",null,"  %gcframe10.sub = getelementptr inbounds [6 x {}*], [6 x {}*]* %gcframe10, i64 0, i64 0")]),i(`
@@ -69,17 +69,17 @@ import{_ as h,C as e,c as k,o as r,aj as d,G as n,w as l,j as s,a as i}from"./ch
 `),s("span",{class:"line"},[s("span",null,"  %box = call noalias nonnull dereferenceable(16) {}* @ijl_gc_pool_alloc(i8* %ptls_load1213, i32 752, i32 16) #9")]),i(`
 `),s("span",{class:"line"},[s("span",null,"  %21 = bitcast {}* %box to i64*")]),i(`
 `),s("span",{class:"line"},[s("span",null,"  %22 = getelementptr inbounds i64, i64* %21, i64 -1")]),i(`
-`),s("span",{class:"line"},[s("span",null,"  store atomic i64 132035716067584, i64* %22 unordered, align 8")]),i(`
+`),s("span",{class:"line"},[s("span",null,"  store atomic i64 129799302689024, i64* %22 unordered, align 8")]),i(`
 `),s("span",{class:"line"},[s("span",null,"  %23 = bitcast {}* %box to double*")]),i(`
 `),s("span",{class:"line"},[s("span",null,"  store double %0, double* %23, align 8")]),i(`
 `),s("span",{class:"line"},[s("span",null,"  %24 = getelementptr inbounds [6 x {}*], [6 x {}*]* %gcframe10, i64 0, i64 5")]),i(`
 `),s("span",{class:"line"},[s("span",null,"  store {}* %box, {}** %24, align 8")]),i(`
-`),s("span",{class:"line"},[s("span",null,"  call void @j_InexactError_6476([3 x {}*]* noalias nocapture noundef nonnull sret([3 x {}*]) %19, {}* inttoptr (i64 132035874784704 to {}*), {}* readonly inttoptr (i64 132035716068384 to {}*), {}* nonnull readonly %box)")]),i(`
+`),s("span",{class:"line"},[s("span",null,"  call void @j_InexactError_6456([3 x {}*]* noalias nocapture noundef nonnull sret([3 x {}*]) %19, {}* inttoptr (i64 129799461406144 to {}*), {}* readonly inttoptr (i64 129799302689824 to {}*), {}* nonnull readonly %box)")]),i(`
 `),s("span",{class:"line"},[s("span",null,"  %ptls_load81415 = load i8*, i8** %20, align 8")]),i(`
 `),s("span",{class:"line"},[s("span",null,"  %box3 = call noalias nonnull dereferenceable(32) {}* @ijl_gc_pool_alloc(i8* %ptls_load81415, i32 800, i32 32) #9")]),i(`
 `),s("span",{class:"line"},[s("span",null,"  %25 = bitcast {}* %box3 to i64*")]),i(`
 `),s("span",{class:"line"},[s("span",null,"  %26 = getelementptr inbounds i64, i64* %25, i64 -1")]),i(`
-`),s("span",{class:"line"},[s("span",null,"  store atomic i64 132035685131632, i64* %26 unordered, align 8")]),i(`
+`),s("span",{class:"line"},[s("span",null,"  store atomic i64 129799271753072, i64* %26 unordered, align 8")]),i(`
 `),s("span",{class:"line"},[s("span",null,"  %27 = bitcast {}* %box3 to i8*")]),i(`
 `),s("span",{class:"line"},[s("span",null,"  %28 = bitcast {}** %18 to i8*")]),i(`
 `),s("span",{class:"line"},[s("span",null,"  call void @llvm.memcpy.p0i8.p0i8.i64(i8* noundef nonnull align 8 dereferenceable(24) %27, i8* noundef nonnull align 16 dereferenceable(24) %28, i64 24, i1 false)")]),i(`
