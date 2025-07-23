56975bbe829eda08f00e30a9388c40ddca0ea638
Anton Smirnov <tonysmn97@gmail.com>
2025-07-23 14:58:52 +0300

    Revert InexactError exception

--- src/device/quirks.jl
+++ src/device/quirks.jl
@@ -1,27 +1,27 @@
 # Copied from CUDA.jl/src/device/quirks.jl
 
 # math.jl
-@device_override Base.Math.throw_complex_domainerror(f::Symbol, x) =
+@device_override @inline Base.Math.throw_complex_domainerror(f::Symbol, x) =
     @gpu_throw "DomainError: This operation requires a complex input to return a complex result"
-@device_override Base.Math.throw_exp_domainerror(f::Symbol, x) =
+@device_override @inline Base.Math.throw_exp_domainerror(f::Symbol, x) =
     @gpu_throw "DomainError: Exponentiation yielding a complex result requires a complex argument"
 
 # intfuncs.jl
-@device_override Base.throw_domerr_powbysq(::Any, p) =
+@device_override @inline Base.throw_domerr_powbysq(::Any, p) =
     @gpu_throw "DomainError: Cannot raise an integer to a negative power"
-@device_override Base.throw_domerr_powbysq(::Integer, p) =
+@device_override @inline Base.throw_domerr_powbysq(::Integer, p) =
     @gpu_throw "DomainError: Cannot raise an integer to a negative power"
-@device_override Base.throw_domerr_powbysq(::AbstractMatrix, p) =
+@device_override @inline Base.throw_domerr_powbysq(::AbstractMatrix, p) =
     @gpu_throw "DomainError: Cannot raise an integer to a negative power"
-@device_override Base.__throw_gcd_overflow(a, b) =
+@device_override @inline Base.__throw_gcd_overflow(a, b) =
     @gpu_throw "OverflowError: GCD overflow"
 
 # checked.jl
-@device_override Base.Checked.throw_overflowerr_binaryop(op, x, y) =
+@device_override @inline Base.Checked.throw_overflowerr_binaryop(op, x, y) =
     @gpu_throw "OverflowError: Binary operation overflowed"
-@device_override Base.Checked.throw_overflowerr_negation(op, x, y) =
+@device_override @inline Base.Checked.throw_overflowerr_negation(op, x, y) =
     @gpu_throw "OverflowError: Negation overflowed"
-@device_override function Base.Checked.checked_abs(x::Base.Checked.SignedInt)
+@device_override @inline function Base.Checked.checked_abs(x::Base.Checked.SignedInt)
     r = ifelse(x < 0, -x, x)
     r < 0 && @gpu_throw "OverflowError: checked arithmetic: cannot compute |x|"
     r
@@ -29,7 +29,9 @@ end
 
 # boot.jl
 @device_override Core.throw_inexacterror(f::Symbol, ::Type{T}, val) where {T} =
-    @gpu_throw "InexactError: Inexact conversion"
+    throw(nothing)
+    # FIXME: https://github.com/JuliaGPU/AMDGPU.jl/issues/808
+    # @gpu_throw "InexactError: Inexact conversion"
 
 # abstractarray.jl
 @device_override Base.throw_boundserror(A, I) =
