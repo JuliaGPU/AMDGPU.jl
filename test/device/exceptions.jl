7f249a311cabbc09d11e4b44b7eb58975247cd47
Anton Smirnov <tonysmn97@gmail.com>
2025-07-23 18:10:30 +0300

    Isolate test to 1.11+

--- test/device/exceptions.jl
+++ test/device/exceptions.jl
@@ -15,21 +15,23 @@
     # TODO check specific exception type
 end
 
-# FIXME: https://github.com/JuliaGPU/AMDGPU.jl/issues/808
-# For now InexactError with just throw(nothing) and not log anything.
-@testset "Issue #808: InexactError failure" begin
-    @inline function add!(a::MMatrix{NC,NC,Complex{T},NC2}) where {NC,NC2,T}
-        ar = reinterpret(reshape, T, a)
-        ar[1] += 1.0
-        return a
-    end
+if VERSION ≥ v"1.11-"
+    # FIXME: https://github.com/JuliaGPU/AMDGPU.jl/issues/808
+    # For now InexactError with just throw(nothing) and not log anything.
+    @testset "Issue #808: InexactError failure" begin
+        @inline function add!(a::MMatrix{NC,NC,Complex{T},NC2}) where {NC,NC2,T}
+            ar = reinterpret(reshape, T, a)
+            ar[1] += 1.0
+            return a
+        end
 
-    function kernel!(a)
-        i = workitemIdx().x
-        a[i] = SMatrix(add!(MMatrix(a[i])))
-        return
-    end
+        function kernel!(a)
+            i = workitemIdx().x
+            a[i] = SMatrix(add!(MMatrix(a[i])))
+            return
+        end
 
-    a = AMDGPU.zeros(SMatrix{3,3,ComplexF64,9}, 1)
-    @roc groupsize=1 kernel!(a)
+        a = AMDGPU.zeros(SMatrix{3,3,ComplexF64,9}, 1)
+        @roc groupsize=1 kernel!(a)
+    end
 end
