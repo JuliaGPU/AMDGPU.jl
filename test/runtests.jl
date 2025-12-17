e0c661bf7de72eba0792c49599c51cc53a3bec66
Christian Guinard <28689358+christiangnrd@users.noreply.github.com>
2025-12-17 13:59:31 -0400

    [temp] Remove once AK with KA 0.10 compat is released

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,6 @@
 @static if VERSION < v"1.11"
     using Pkg
+    Pkg.add(url="https://github.com/christiangnrd/AcceleratedKernels.jl", rev="ka0.10simple")
     Pkg.add(url="https://github.com/JuliaGPU/KernelAbstractions.jl", rev="main")
 end
 
