c79ccae75c7a07f25192b6d91d2187b62c5e79c6
Katharine Hyatt <katharine.hyatt@ugent.be>
2026-03-10 12:02:07 +0100

    Don't test hiptensor if it's not available

--- test/hiptensor/elementwise_binary.jl
+++ test/hiptensor/elementwise_binary.jl
@@ -3,123 +3,126 @@ using AMDGPU
 using LinearAlgebra
 using AMDGPU.hipTENSOR: elementwise_binary_execute!, hipTensor
 
-AMDGPU.hipTENSOR.hiptensorLoggerSetLevel(AMDGPU.hipTENSOR.hiptensorLogLevel_t(UInt32(16)))
-AMDGPU.hipTENSOR.hiptensorLoggerOpenFile("tensor.log")
+if AMDGPU.hipTENSOR.has_hiptensor()
 
-eltypes = [#(Float16, Float16),
-           #(Float32, Float32),
-           (Float64, Float64),
-           #=(ComplexF32, ComplexF32),
-           (ComplexF64, ComplexF64),
-           (ComplexF64, ComplexF32),
-           (Float32, Float16),
-           (Float64, Float32),=#
-           ]
+    AMDGPU.hipTENSOR.hiptensorLoggerSetLevel(AMDGPU.hipTENSOR.hiptensorLogLevel_t(UInt32(16)))
+    AMDGPU.hipTENSOR.hiptensorLoggerOpenFile("tensor.log")
 
-Ns = 2:5
+    eltypes = [#(Float16, Float16),
+               #(Float32, Float32),
+               (Float64, Float64),
+               #=(ComplexF32, ComplexF32),
+               (ComplexF64, ComplexF64),
+               (ComplexF64, ComplexF32),
+               (Float32, Float16),
+               (Float64, Float32),=#
+               ]
 
-@testset "elementwise binary ($eltyA, $eltyC), N $N" for (eltyA, eltyC) in eltypes, N in Ns
-    # setup
-    eltyD = eltyC
-    #dmax  = 2^div(18,N)
-    #dims  = rand(2:dmax, N)
-    dims  = fill(4, N)
-    p     = randperm(N)
-    indsA = collect(('a':'z')[1:N])
-    indsC = indsA[p]
-    dimsA = dims
-    dimsC = dims[p]
-    A     = rand(eltyA, dimsA...)
-    dA    = ROCArray(A)
-    C     = rand(eltyC, dimsC...)
-    dC    = ROCArray(C)
-    AMDGPU.synchronize()
+    Ns = 2:5
 
-    @testset "simple case" begin
-        opA   = AMDGPU.hipTENSOR.OP_IDENTITY
-        opC   = AMDGPU.hipTENSOR.OP_IDENTITY
-        dD    = similar(dC, eltyD)
-        opAC  = AMDGPU.hipTENSOR.OP_ADD
-        dD    = elementwise_binary_execute!(one(eltyA), dA, indsA, opA, one(eltyC), dC, indsC, opC, dD, indsC, opAC)
-        D     = collect(dD)
-        @test D ≈ permutedims(A, p) + C
-    end
+    @testset "elementwise binary ($eltyA, $eltyC), N $N" for (eltyA, eltyC) in eltypes, N in Ns
+        # setup
+        eltyD = eltyC
+        #dmax  = 2^div(18,N)
+        #dims  = rand(2:dmax, N)
+        dims  = fill(4, N)
+        p     = randperm(N)
+        indsA = collect(('a':'z')[1:N])
+        indsC = indsA[p]
+        dimsA = dims
+        dimsC = dims[p]
+        A     = rand(eltyA, dimsA...)
+        dA    = ROCArray(A)
+        C     = rand(eltyC, dimsC...)
+        dC    = ROCArray(C)
+        AMDGPU.synchronize()
 
-    #=@testset "using integers as indices" begin
-        opA   = AMDGPU.hipTENSOR.OP_IDENTITY
-        opC   = AMDGPU.hipTENSOR.OP_IDENTITY
-        dD    = similar(dC, eltyD)
-        opAC  = AMDGPU.hipTENSOR.OP_ADD
-        dD    = elementwise_binary_execute!(1, dA, 1:N, opA, 1, dC, p, opC, dD, p, opAC)
-        D     = collect(dD)
-        @test D ≈ permutedims(A, p) + C
-    end
-
-    @testset "multiplication as binary operator" begin
-        opA   = AMDGPU.hipTENSOR.OP_IDENTITY
-        opC   = AMDGPU.hipTENSOR.OP_IDENTITY
-        dD    = similar(dC, eltyD)
-        opAC  = AMDGPU.hipTENSOR.OP_MUL
-        dD    = elementwise_binary_execute!(1, dA, indsA, opA, 1, dC, indsC, opC, dD, indsC, opAC)
-        D     = collect(dD)
-        @test D ≈ permutedims(A, p) .* C
-    end
-
-    @testset "with non-trivial coefficients and conjugation" begin
-        dD   = similar(dC, eltyD)
-        opA  = eltyA <: Complex ? AMDGPU.hipTENSOR.OP_CONJ : AMDGPU.hipTENSOR.OP_IDENTITY
-        opC  = AMDGPU.hipTENSOR.OP_IDENTITY
-        opAC = AMDGPU.hipTENSOR.OP_ADD
-        α    = rand(eltyD)
-        γ    = rand(eltyD)
-        dD   = elementwise_binary_execute!(α, dA, indsA, opA, γ, dC, indsC, opC, dD, indsC, opAC)
-        D    = collect(dD)
-        @test D ≈ α * conj.(permutedims(A, p)) + γ * C
-    end
-
-    @testset "test in-place, and more complicated unary and binary operations" begin
-        opA = eltyA <: Complex ? AMDGPU.hipTENSOR.OP_IDENTITY : AMDGPU.hipTENSOR.OP_SQRT
-        # because we use rand, entries of A will be positive when elty is real
-        opC = eltyC <: Complex ? AMDGPU.hipTENSOR.OP_CONJ : AMDGPU.hipTENSOR.OP_IDENTITY
-        opAC = eltyD <: Complex ? AMDGPU.hipTENSOR.OP_ADD : AMDGPU.hipTENSOR.OP_MAX
-        α = rand(eltyD)
-        γ = rand(eltyD)
-        dD = elementwise_binary_execute!(α, dA, indsA, opA, γ, dC, indsC, opC, dC, indsC, opAC)
-        D = collect(dC)
-        if eltyD <: Complex
-            if eltyA <: Complex
-                @test D ≈ α * permutedims(A, p) + γ * conj.(C)
-            else
-                @test D ≈ α * sqrt.(eltyD.(permutedims(A, p))) + γ * conj.(C)
-            end
-        else
-            @test D ≈ max.(α * sqrt.(eltyD.(permutedims(A, p))), γ * C)
+        @testset "simple case" begin
+            opA   = AMDGPU.hipTENSOR.OP_IDENTITY
+            opC   = AMDGPU.hipTENSOR.OP_IDENTITY
+            dD    = similar(dC, eltyD)
+            opAC  = AMDGPU.hipTENSOR.OP_ADD
+            dD    = elementwise_binary_execute!(one(eltyA), dA, indsA, opA, one(eltyC), dC, indsC, opC, dD, indsC, opAC)
+            D     = collect(dD)
+            @test D ≈ permutedims(A, p) + C
         end
+
+        #=@testset "using integers as indices" begin
+            opA   = AMDGPU.hipTENSOR.OP_IDENTITY
+            opC   = AMDGPU.hipTENSOR.OP_IDENTITY
+            dD    = similar(dC, eltyD)
+            opAC  = AMDGPU.hipTENSOR.OP_ADD
+            dD    = elementwise_binary_execute!(1, dA, 1:N, opA, 1, dC, p, opC, dD, p, opAC)
+            D     = collect(dD)
+            @test D ≈ permutedims(A, p) + C
+        end
+
+        @testset "multiplication as binary operator" begin
+            opA   = AMDGPU.hipTENSOR.OP_IDENTITY
+            opC   = AMDGPU.hipTENSOR.OP_IDENTITY
+            dD    = similar(dC, eltyD)
+            opAC  = AMDGPU.hipTENSOR.OP_MUL
+            dD    = elementwise_binary_execute!(1, dA, indsA, opA, 1, dC, indsC, opC, dD, indsC, opAC)
+            D     = collect(dD)
+            @test D ≈ permutedims(A, p) .* C
+        end
+
+        @testset "with non-trivial coefficients and conjugation" begin
+            dD   = similar(dC, eltyD)
+            opA  = eltyA <: Complex ? AMDGPU.hipTENSOR.OP_CONJ : AMDGPU.hipTENSOR.OP_IDENTITY
+            opC  = AMDGPU.hipTENSOR.OP_IDENTITY
+            opAC = AMDGPU.hipTENSOR.OP_ADD
+            α    = rand(eltyD)
+            γ    = rand(eltyD)
+            dD   = elementwise_binary_execute!(α, dA, indsA, opA, γ, dC, indsC, opC, dD, indsC, opAC)
+            D    = collect(dD)
+            @test D ≈ α * conj.(permutedims(A, p)) + γ * C
+        end
+
+        @testset "test in-place, and more complicated unary and binary operations" begin
+            opA = eltyA <: Complex ? AMDGPU.hipTENSOR.OP_IDENTITY : AMDGPU.hipTENSOR.OP_SQRT
+            # because we use rand, entries of A will be positive when elty is real
+            opC = eltyC <: Complex ? AMDGPU.hipTENSOR.OP_CONJ : AMDGPU.hipTENSOR.OP_IDENTITY
+            opAC = eltyD <: Complex ? AMDGPU.hipTENSOR.OP_ADD : AMDGPU.hipTENSOR.OP_MAX
+            α = rand(eltyD)
+            γ = rand(eltyD)
+            dD = elementwise_binary_execute!(α, dA, indsA, opA, γ, dC, indsC, opC, dC, indsC, opAC)
+            D = collect(dC)
+            if eltyD <: Complex
+                if eltyA <: Complex
+                    @test D ≈ α * permutedims(A, p) + γ * conj.(C)
+                else
+                    @test D ≈ α * sqrt.(eltyD.(permutedims(A, p))) + γ * conj.(C)
+                end
+            else
+                @test D ≈ max.(α * sqrt.(eltyD.(permutedims(A, p))), γ * C)
+            end
+        end
+
+        # using hipTensor type
+        dA = ROCArray(A)
+        dC = ROCArray(C)
+        ctA = hipTensor(dA, indsA)
+        ctC = hipTensor(dC, indsC)
+        ctD = ctA + ctC
+        hD = collect(ctD.data)
+        @test hD ≈ permutedims(A, p) + C
+        ctD = ctA - ctC
+        hD = collect(ctD.data)
+        @test hD ≈ permutedims(A, p) - C
+
+        α = rand(eltyD)
+        ctC_copy = copy(ctC)
+        ctD = LinearAlgebra.axpy!(α, ctA, ctC_copy)
+        @test ctD == ctC_copy
+        hD = collect(ctD.data)
+        @test hD ≈ α * permutedims(A, p) + C
+
+        γ = rand(eltyD)
+        ctC_copy = copy(ctC)
+        ctD = LinearAlgebra.axpby!(α, ctA, γ, ctC_copy)
+        @test ctD == ctC_copy
+        hD = collect(ctD.data)
+        @test hD ≈ α * permutedims(A, p) + γ * C=#
     end
-
-    # using hipTensor type
-    dA = ROCArray(A)
-    dC = ROCArray(C)
-    ctA = hipTensor(dA, indsA)
-    ctC = hipTensor(dC, indsC)
-    ctD = ctA + ctC
-    hD = collect(ctD.data)
-    @test hD ≈ permutedims(A, p) + C
-    ctD = ctA - ctC
-    hD = collect(ctD.data)
-    @test hD ≈ permutedims(A, p) - C
-
-    α = rand(eltyD)
-    ctC_copy = copy(ctC)
-    ctD = LinearAlgebra.axpy!(α, ctA, ctC_copy)
-    @test ctD == ctC_copy
-    hD = collect(ctD.data)
-    @test hD ≈ α * permutedims(A, p) + C
-
-    γ = rand(eltyD)
-    ctC_copy = copy(ctC)
-    ctD = LinearAlgebra.axpby!(α, ctA, γ, ctC_copy)
-    @test ctD == ctC_copy
-    hD = collect(ctD.data)
-    @test hD ≈ α * permutedims(A, p) + γ * C=#
 end
--- test/hiptensor/permutations.jl
+++ test/hiptensor/permutations.jl
@@ -1,52 +1,55 @@
 using Test, AMDGPU
 using LinearAlgebra, Random
 
-@testset "permutations" begin
+if AMDGPU.hipTENSOR.has_hiptensor()
 
-using AMDGPU.hipTENSOR: permute!
+    @testset "permutations" begin
 
-AMDGPU.hipTENSOR.hiptensorLoggerSetLevel(AMDGPU.hipTENSOR.hiptensorLogLevel_t(UInt32(16)))
-AMDGPU.hipTENSOR.hiptensorLoggerOpenFile("tensor.log")
+    using AMDGPU.hipTENSOR: permute!
 
-eltypes = [(Float16, Float16),
-           #=(Float16, Float32),
-           (Float32, Float16),
-           (Float32, Float32),
-           (Float64, Float64),
-           (Float32, Float64),
-           (Float64, Float32),
-           (ComplexF32, ComplexF32),
-           (ComplexF64, ComplexF64),
-           (ComplexF32, ComplexF64),
-           (ComplexF64, ComplexF32)=#
-           ]
+    AMDGPU.hipTENSOR.hiptensorLoggerSetLevel(AMDGPU.hipTENSOR.hiptensorLogLevel_t(UInt32(16)))
+    AMDGPU.hipTENSOR.hiptensorLoggerOpenFile("tensor.log")
 
-@testset for N=2#:5
-    @testset for (eltyA, eltyC) in eltypes
-        # setup
-        dmax = 2^div(18,N)
-        dims = rand(2:dmax, N)
-        p = randperm(N)
-        indsA = collect(('a':'z')[1:N])
-        indsC = indsA[p]
-        dimsA = dims
-        dimsC = dims[p]
-        A = rand(eltyA, dimsA...)
-        dA = ROCArray(A)
-        dC = similar(dA, eltyC, dimsC...)
+    eltypes = [(Float16, Float16),
+               #=(Float16, Float32),
+               (Float32, Float16),
+               (Float32, Float32),
+               (Float64, Float64),
+               (Float32, Float64),
+               (Float64, Float32),
+               (ComplexF32, ComplexF32),
+               (ComplexF64, ComplexF64),
+               (ComplexF32, ComplexF64),
+               (ComplexF64, ComplexF32)=#
+               ]
 
-        # simple case
-        opA = AMDGPU.hipTENSOR.OP_IDENTITY
-        dC = permute!(one(eltyA), dA, indsA, opA, dC, indsC)
-        C  = collect(dC)
-        @test C == permutedims(A, p) # exact equality
+    @testset for N=2#:5
+        @testset for (eltyA, eltyC) in eltypes
+            # setup
+            dmax = 2^div(18,N)
+            dims = rand(2:dmax, N)
+            p = randperm(N)
+            indsA = collect(('a':'z')[1:N])
+            indsC = indsA[p]
+            dimsA = dims
+            dimsC = dims[p]
+            A = rand(eltyA, dimsA...)
+            dA = ROCArray(A)
+            dC = similar(dA, eltyC, dimsC...)
 
-        # with scalar
-        α  = rand(eltyA)
-        dC = permute!(α, dA, indsA, opA, dC, indsC)
-        C  = collect(dC)
-        @test C ≈ α * permutedims(A, p) # approximate, floating point rounding
+            # simple case
+            opA = AMDGPU.hipTENSOR.OP_IDENTITY
+            dC = permute!(one(eltyA), dA, indsA, opA, dC, indsC)
+            C  = collect(dC)
+            @test C == permutedims(A, p) # exact equality
+
+            # with scalar
+            α  = rand(eltyA)
+            dC = permute!(α, dA, indsA, opA, dC, indsC)
+            C  = collect(dC)
+            @test C ≈ α * permutedims(A, p) # approximate, floating point rounding
+        end
     end
-end
 
+    end
 end
