21c9fc67e27137a92105a77baf8ed857b2f68358
Anton Smirnov <tonysmn97@gmail.com>
2026-03-14 21:02:35 +0200

    Modularize WMMA

--- src/device/gcn/wmma.jl
+++ src/device/gcn/wmma.jl
@@ -1,11 +1,19 @@
 # WMMA (Wavefront Matrix Multiply-Accumulate) intrinsics for RDNA 3 (GFX11)
 # https://github.com/llvm/llvm-project/blob/main/llvm/test/CodeGen/AMDGPU/llvm.amdgcn.wmma_32.ll
 
-export WMMAFragment, WMMA_M, WMMA_N, WMMA_K
-export wmma_load_a, wmma_load_b, wmma_load_c, wmma_store_d, wmma_mma
+export WMMA
+
+module WMMA
+
+export Fragment, M, N, K
+export load_a, load_b, load_c, store_d, mma, fill_c
+
+import Core: LLVMPtr, VecElement
+import ...BFloat16s: BFloat16
+import ..activelane
 
 # WMMA tile dimensions (fixed for RDNA 3).
-const WMMA_M, WMMA_N, WMMA_K = 16, 16, 16
+const M, N, K = 16, 16, 16
 
 const LLVMVec16F16 = NTuple{16, VecElement{Float16}}
 const LLVMVec16BF16 = NTuple{16, VecElement{BFloat16}}
@@ -13,7 +21,7 @@ const LLVMVec16I16 = NTuple{16, VecElement{Int16}}
 const LLVMVec8F32 = NTuple{8, VecElement{Float32}}
 
 """
-    WMMAFragment{M, N, T, L}
+    Fragment{M, N, T, L}
 
 A fragment of a matrix for WMMA operations.
 - `M`, `N`: logical matrix dimensions this fragment represents a piece of
@@ -25,22 +33,22 @@ For wave32 mode on RDNA 3:
 - B fragment (Kx16): 16 elements per thread (8 VGPRs for FP16)
 - C/D fragment (16x16): 8 elements per thread (8 VGPRs for FP32, or 8 VGPRs holding 16 FP16)
 """
-struct WMMAFragment{M, N, T, L}
+struct Fragment{M, N, T, L}
     data::NTuple{L, VecElement{T}}
 end
 
-const WMMAFragmentA{T} = WMMAFragment{WMMA_M, WMMA_K, T, 16}
-const WMMAFragmentB{T} = WMMAFragment{WMMA_K, WMMA_N, T, 16}
-const WMMAFragmentC_F32 = WMMAFragment{WMMA_M, WMMA_N, Float32, 8}
-const WMMAFragmentC_F16 = WMMAFragment{WMMA_M, WMMA_N, Float16, 16}
-const WMMAFragmentC_BF16 = WMMAFragment{WMMA_M, WMMA_N, BFloat16, 16}
+const FragmentA{T} = Fragment{M, K, T, 16}
+const FragmentB{T} = Fragment{K, N, T, 16}
+const FragmentC_F32 = Fragment{M, N, Float32, 8}
+const FragmentC_F16 = Fragment{M, N, Float16, 16}
+const FragmentC_BF16 = Fragment{M, N, BFloat16, 16}
 
 """
-    wmma_fill_c(::Type{Float32}, x::Float32)
+    fill_c(::Type{Float32}, x::Float32)
 
 Create and return fragment for `C` filled with given value `x`.
 """
-wmma_fill_c(::Type{Float32}, x::Float32) = WMMAFragmentC_F32(ntuple(_ -> VecElement(x), Val(8)))
+fill_c(::Type{Float32}, x::Float32) = FragmentC_F32(ntuple(_ -> VecElement(x), Val(8)))
 
 # Signature for BFloat16 is:
 # @llvm.amdgcn.wmma.f32.16x16x16.bf16(<16 x i16>, <16 x i16> , <8 x float>)
@@ -51,8 +59,8 @@ _llvm_inttype(::Type{Float16}) = Float16
 _llvm_inttype(::Type{BFloat16}) = Int16
 
 for (fname, intrinsic, ret_T, arg_T) in (
-    (:_wmma_f32_16x16x16_f16, "llvm.amdgcn.wmma.f32.16x16x16.f16", LLVMVec8F32, LLVMVec16F16),
-    (:_wmma_f32_16x16x16_bf16, "llvm.amdgcn.wmma.f32.16x16x16.bf16", LLVMVec8F32, LLVMVec16I16),
+    (:_f32_16x16x16_f16, "llvm.amdgcn.wmma.f32.16x16x16.f16", LLVMVec8F32, LLVMVec16F16),
+    (:_f32_16x16x16_bf16, "llvm.amdgcn.wmma.f32.16x16x16.bf16", LLVMVec8F32, LLVMVec16I16),
 )
     @eval $fname(a::$arg_T, b::$arg_T, c::$ret_T) = ccall(
         $intrinsic, llvmcall, $ret_T,
@@ -60,33 +68,33 @@ for (fname, intrinsic, ret_T, arg_T) in (
 end
 
 """
-    wmma_load_a(ptr::LLVMPtr{T}, stride::Int32) where T <: Union{Float16, BFloat16}
-    wmma_load_b(ptr::LLVMPtr{T}, stride::Int32) where T <: Union{Float16, BFloat16}
-    wmma_load_c(ptr::LLVMPtr{Float32}, stride::Int32)
+    load_a(ptr::LLVMPtr{T}, stride::Int32) where T <: Union{Float16, BFloat16}
+    load_b(ptr::LLVMPtr{T}, stride::Int32) where T <: Union{Float16, BFloat16}
+    load_c(ptr::LLVMPtr{Float32}, stride::Int32)
 
 Load the matrix `A`, `B` or `C` from the memory given by `ptr` and return the resulting fragment.
 """
-function wmma_load_a(ptr::LLVMPtr{T}, stride::Int32) where T <: Union{Float16, BFloat16}
+function load_a(ptr::LLVMPtr{T}, stride::Int32) where T <: Union{Float16, BFloat16}
     lane = unsafe_trunc(Int32, activelane())
     row = lane & Int32(15)
     data = ntuple(Val(16)) do col
         offset = (Int32(col - 1) * stride + row) * Int32(sizeof(T))
         VecElement(_llvm_inttype(unsafe_load(ptr + offset)))
     end
-    return WMMAFragmentA{_llvm_inttype(T)}(data)
+    return FragmentA{_llvm_inttype(T)}(data)
 end
 
-function wmma_load_b(ptr::LLVMPtr{T}, stride::Int32) where T <: Union{Float16, BFloat16}
+function load_b(ptr::LLVMPtr{T}, stride::Int32) where T <: Union{Float16, BFloat16}
     lane = unsafe_trunc(Int32, activelane())
     col = lane & Int32(15)
     base = ptr + col * stride * Int32(sizeof(T))
     data = ntuple(Val(16)) do row
         VecElement(_llvm_inttype(unsafe_load(base + Int32(row - 1) * Int32(sizeof(T)))))
     end
-    return WMMAFragmentB{_llvm_inttype(T)}(data)
+    return FragmentB{_llvm_inttype(T)}(data)
 end
 
-function wmma_load_c(ptr::LLVMPtr{Float32}, stride::Int32)::WMMAFragmentC_F32
+function load_c(ptr::LLVMPtr{Float32}, stride::Int32)::FragmentC_F32
     lane = unsafe_trunc(Int32, activelane())
     col = lane & Int32(15)
     half = lane >> 4
@@ -95,11 +103,11 @@ function wmma_load_c(ptr::LLVMPtr{Float32}, stride::Int32)::WMMAFragmentC_F32
         offset = (col * stride + row) * Int32(sizeof(Float32))
         VecElement(_llvm_inttype(unsafe_load(ptr + offset)))
     end
-    return WMMAFragmentC_F32(data)
+    return FragmentC_F32(data)
 end
 
 """
-    wmma_store_d(ptr::LLVMPtr{Float32}, frag::WMMAFragmentC_F32, stride::Int32)
+    store_d(ptr::LLVMPtr{Float32}, frag::FragmentC_F32, stride::Int32)
 
 Store the result matrix `D` to the memory location given by `ptr`.
 
@@ -109,7 +117,7 @@ Store the result matrix `D` to the memory location given by `ptr`.
 - `frag`: Corresponding fragment.
 - `stride`: Leading dimension of the matrix for `ptr` in number of elements.
 """
-function wmma_store_d(ptr::LLVMPtr{Float32}, frag::WMMAFragmentC_F32, stride::Int32)
+function store_d(ptr::LLVMPtr{Float32}, frag::FragmentC_F32, stride::Int32)
     lane = unsafe_trunc(Int32, activelane())
     col = lane & Int32(15)
     half = lane >> 4
@@ -122,16 +130,18 @@ function wmma_store_d(ptr::LLVMPtr{Float32}, frag::WMMAFragmentC_F32, stride::In
 end
 
 """
-    wmma_mma(
-        a::WMMAFragmentA{T}, b::WMMAFragmentB{T}, c::WMMAFragmentC_F32,
+    mma(
+        a::FragmentA{T}, b::FragmentB{T}, c::FragmentC_F32,
     ) where T <: Union{Float16, BFloat16}
 
 Perform matrix multiply-accumulate operation `D = A ⋅ B + C` with loaded fragments.
 `A` and `B` can be either in `Float16` or in `BFloat16`.
 """
-wmma_mma
+mma
 
-for (fname, arg_T) in ((_wmma_f32_16x16x16_f16, Float16), (_wmma_f32_16x16x16_bf16, Int16))
-    @eval wmma_mma(a::WMMAFragmentA{$arg_T}, b::WMMAFragmentB{$arg_T}, c::WMMAFragmentC_F32) =
-        WMMAFragmentC_F32($fname(a.data, b.data, c.data))
+for (fname, arg_T) in ((_f32_16x16x16_f16, Float16), (_f32_16x16x16_bf16, Int16))
+    @eval mma(a::FragmentA{$arg_T}, b::FragmentB{$arg_T}, c::FragmentC_F32) =
+        FragmentC_F32($fname(a.data, b.data, c.data))
 end
+
+end # module WMMA
--- test/wmma_tests.jl
+++ test/wmma_tests.jl
@@ -2,8 +2,7 @@ using Test
 using AMDGPU
 using AMDGPU: ROCArray, @roc
 using BFloat16s
-using AMDGPU.Device: wmma_load_a, wmma_load_b, wmma_load_c, wmma_store_d, wmma_mma, wmma_fill_c
-using AMDGPU.Device: WMMA_M, WMMA_N, WMMA_K, workitemIdx, workgroupIdx
+using AMDGPU.Device: WMMA, workitemIdx, workgroupIdx
 
 AMDGPU.allowscalar(false)
 
@@ -15,32 +14,32 @@ if !is_rdna3
 else
 
 function wmma_kernel_ptr!(C, A::AbstractArray{T}, B, M::Int32, N::Int32, K::Int32) where T
-    tile_row = (workgroupIdx().x - Int32(1)) * Int32(WMMA_M)
-    tile_col = (workgroupIdx().y - Int32(1)) * Int32(WMMA_N)
+    tile_row = (workgroupIdx().x - Int32(1)) * Int32(WMMA.M)
+    tile_col = (workgroupIdx().y - Int32(1)) * Int32(WMMA.N)
 
     C_ptr = pointer(C)
     A_ptr = pointer(A)
     B_ptr = pointer(B)
 
-    c_frag = wmma_fill_c(Float32, 0f0)
+    c_frag = WMMA.fill_c(Float32, 0f0)
     k = Int32(0)
     while k < K
         a_ptr = A_ptr + (k * M + tile_row) * Int32(sizeof(T))
         b_ptr = B_ptr + (tile_col * K + k) * Int32(sizeof(T))
 
-        a_frag = wmma_load_a(a_ptr, M)
-        b_frag = wmma_load_b(b_ptr, K)
-        c_frag = wmma_mma(a_frag, b_frag, c_frag)
+        a_frag = WMMA.load_a(a_ptr, M)
+        b_frag = WMMA.load_b(b_ptr, K)
+        c_frag = WMMA.mma(a_frag, b_frag, c_frag)
 
-        k += Int32(WMMA_K)
+        k += Int32(WMMA.K)
     end
 
     c_ptr = C_ptr + (tile_col * M + tile_row) * Int32(sizeof(Float32))
-    wmma_store_d(c_ptr, c_frag, M)
+    WMMA.store_d(c_ptr, c_frag, M)
     return
 end
 
-@testset "core: WMMA" begin
+@testset "WMMA" begin
 
     @testset "Tiled $M x $N: $arg_T" for (M, N, K) in (
         (64, 64, 64), (128, 128, 128),
@@ -50,7 +49,7 @@ end
         A, B = ROCArray(A_host), ROCArray(B_host)
         C = ROCArray(zeros(Float32, M, N))
 
-        tiles_m, tiles_n = M ÷ WMMA_M, N ÷ WMMA_N
+        tiles_m, tiles_n = M ÷ WMMA.M, N ÷ WMMA.N
         @roc gridsize=(tiles_m, tiles_n) groupsize=32 wmma_kernel_ptr!(
             C, A, B, Int32(M), Int32(N), Int32(K))
         @test maximum(abs.(Float32.(C) .- (Float32.(A) * Float32.(B)))) < 0.1
