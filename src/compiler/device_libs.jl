38529710fb1a53b2e73af334b2412443640b8075
Ludovic Raess <ludovic.rass@gmail.com>
2026-05-11 10:47:34 +0200

    Rely on LLVM

--- src/compiler/device_libs.jl
+++ src/compiler/device_libs.jl
@@ -16,20 +16,18 @@ mutable struct DevLib
     name::String
     path::String
     data::Vector{UInt8}
-    fn_names::Set{String}
 
-    DevLib(name::String, path::String) = new(name, path, read(path), Set{String}())
-    DevLib(name::String, ::Nothing) = new(name, "", UInt8[], Set{String}())
+    DevLib(name::String, path::String) = new(name, path, read(path))
+    DevLib(name::String, ::Nothing) = new(name, "", UInt8[])
 end
 
 const DEVICE_LIBS::Dict{String, DevLib} = Dict{String, DevLib}()
 
 function link_device_libs!(
-    target::GCNCompilerTarget, mod::LLVM.Module, undefined_fns::Vector{String};
-    wavefrontsize64::Bool, only_undefined::Bool,
+    target::GCNCompilerTarget, mod::LLVM.Module;
+    wavefrontsize64::Bool,
 )
     isnothing(libdevice_libs) && return
-    isempty(undefined_fns) && return
 
     # 1. Load other libraries.
     lib_names = ("hc", "hip", "irif", "ockl", "opencl", "ocml")
@@ -37,9 +35,8 @@ function link_device_libs!(
         devlib = get!(DEVICE_LIBS, lib_name) do
             DevLib(lib_name, locate_lib(lib_name))
         end
-        load_and_link!(devlib, mod, undefined_fns)
+        load_and_link!(devlib, mod)
     end
-    only_undefined && return
 
     # 2. Load OCLC library.
     devlib = get!(DEVICE_LIBS, "oclc") do
@@ -72,20 +69,8 @@ function link_device_libs!(
     end
 end
 
-function load_and_link!(
-    devlib::DevLib, mod::LLVM.Module, undefined_fns::Vector{String} = String[],
-)
+function load_and_link!(devlib::DevLib, mod::LLVM.Module)
     isempty(devlib.path) && return
-
-    fill_fn_names = isempty(devlib.fn_names)
-    do_linking = false
-
-    if !fill_fn_names && !isempty(undefined_fns)
-        for undef_fn in undefined_fns
-            undef_fn ∈ devlib.fn_names && (do_linking = true; break)
-        end
-        do_linking || return
-    end
 
     lib = parse(LLVM.Module, devlib.data)
     inline_attr = EnumAttribute("alwaysinline")
@@ -93,7 +78,6 @@ function load_and_link!(
 
     for f in LLVM.functions(lib)
         fn_name = LLVM.name(f)
-        fill_fn_names && push!(devlib.fn_names, fn_name)
 
         # FIXME: We should be able to inline this, that we can't means
         #        we are inserting calls to it late.
@@ -110,16 +94,9 @@ function load_and_link!(
         inline && push!(attrs, inline_attr)
     end
 
-    if !do_linking && !isempty(undefined_fns)
-        for undef_fn in undefined_fns
-            undef_fn ∈ devlib.fn_names && (do_linking = true; break)
-        end
-        do_linking || return
-    end
-
     # override triple and datalayout to avoid warnings
     triple!(lib, triple(mod))
     datalayout!(lib, datalayout(mod))
-    LLVM.link!(mod, lib)
+    LLVM.link!(mod, lib; only_needed=true)
     return
 end
