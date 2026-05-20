8e16a715f3004e49ce69bdd2dd7a0a46a56e2241
Simeon David Schaub <simeon@schaub.rocks>
2026-05-20 19:15:24 +0200

    introduce custom comparison strategy

--- .pkg/platform_augmentation.jl
+++ .pkg/platform_augmentation.jl
@@ -16,7 +16,7 @@ function rocm_arch()
             target = parse(Int, m[1])
             target == 0 && continue
 
-            push!(arch, "gfx$(div(target, 10000))$(div(target, 100) % 100)$(target % 100)")
+            push!(arch, rocm_arch_string(target))
         end
     end
 
@@ -25,16 +25,43 @@ function rocm_arch()
     return arch
 end
 
+function rocm_arch_string(target::Integer)
+    patch = lowercase(string(target % 100, base = 16))
+    return "gfx$(div(target, 10000))$(div(target, 100) % 100)$(patch)"
+end
+
 function rocm_arch_comparison_strategy(a::String, b::String, a_requested::Bool, b_requested::Bool)
     a == "none" && return false
     b == "none" && return false
 
     a_arches = split(a, ',')
     b_arches = split(b, ',')
-    for arch in a_arches
-        arch in b_arches && return true
+    for a_arch in a_arches
+        for b_arch in b_arches
+            rocm_arch_matches(a_arch, b_arch) && return true
+            rocm_arch_matches(b_arch, a_arch) && return true
+        end
     end
     return false
+end
+
+function rocm_arch_core(arch::AbstractString)
+    return first(split(arch, r"[_-]", limit = 2))
+end
+
+function rocm_arch_matches(pattern::AbstractString, arch::AbstractString)
+    pattern = rocm_arch_core(pattern)
+    arch = rocm_arch_core(arch)
+
+    length(pattern) == length(arch) || return false
+    for (pattern_char, arch_char) in zip(pattern, arch)
+        if pattern_char == 'X'
+            isxdigit(arch_char) || return false
+        elseif pattern_char != arch_char
+            return false
+        end
+    end
+    return true
 end
 
 function augment_platform!(platform::Platform)
