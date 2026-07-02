3e816f5b6903344ddfda1f13941df4d44392bb20
Christian Guinard <28689358+christiangnrd@users.noreply.github.com>
2026-07-02 11:03:36 -0300

    KA.versioninfo()

--- src/ROCKernels.jl
+++ src/ROCKernels.jl
@@ -23,6 +23,8 @@ obtain it from an array with `KernelAbstractions.get_backend(::ROCArray)`.
 """
 struct ROCBackend <: KA.GPU end
 
+KA.versioninfo(io::IO, ::ROCBackend) = AMDGPU.versioninfo(io)
+
 KA.functional(::ROCBackend) = AMDGPU.functional()
 KA.ndevices(::ROCBackend) = AMDGPU.HIP.ndevices()
 KA.device(::ROCBackend) = AMDGPU.device_id()
