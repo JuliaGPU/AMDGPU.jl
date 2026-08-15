726f1b559d6ed0369378b91dd2b29af30a99ddc8
Ka Wai Ho <doraemonho@login1.odo.olcf.ornl.gov>
2026-08-15 00:19:10 -0400

    compatibility fix applied to julia 1.10

--- src/device/gcn/synchronization.jl
+++ src/device/gcn/synchronization.jl
@@ -36,7 +36,7 @@ Identical to `sync_workgroup`, with the additional feature
 that it evaluates the predicate for all workitems in the workgroup
 and returns the number of workitems for which predicate evaluates to non-zero.
 """
-@device_function @inline function sync_workgroup_count(predicate::Cint)::Cint
+@device_function @inline function sync_workgroup_count(predicate::Cint)
     ccall(
         "extern __ockl_wgred_add_i32", llvmcall, Cint, (Cint,),
         __not(__not(predicate)))
@@ -49,7 +49,7 @@ Identical to `sync_workgroup`, with the additional feature
 that it evaluates the predicate for all workitems in the workgroup
 and returns non-zero if and only if predicate evaluates to non-zero for all of them.
 """
-@device_function @inline function sync_workgroup_and(predicate::Cint)::Cint
+@device_function @inline function sync_workgroup_and(predicate::Cint)
     ccall(
         "extern __ockl_wgred_and_i32", llvmcall, Cint, (Cint,),
         __not(__not(predicate)))
@@ -62,7 +62,7 @@ Identical to `sync_workgroup`, with the additional feature
 that it evaluates the predicate for all workitems in the workgroup
 and returns non-zero if and only if predicate evaluates to non-zero for any of them.
 """
-@device_function @inline function sync_workgroup_or(predicate::Cint)::Cint
+@device_function @inline function sync_workgroup_or(predicate::Cint)
     ccall(
         "extern __ockl_wgred_or_i32", llvmcall, Cint, (Cint,),
         __not(__not(predicate)))
@@ -70,6 +70,6 @@ end
 
 @inline __not(x::Cint)::Cint = ifelse(iszero(x), one(x), zero(x))
 
-@device_function @inline function sync_grid()::Cvoid
+@device_function @inline function sync_grid()
     ccall("extern __ockl_grid_sync", llvmcall, Cvoid, ())
 end
